90f908f8297a3e7d6df2c1cb88482dcfa09d7f93
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 12:59:03 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -21,7 +21,7 @@ const { Color } = require("../../config.js");
           if(!user || !args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage: s!ban [@User]`));
           if(message.mentions.users.size < 1) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Noy found this member`));
           if(!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`This member have role highst me can't ban`));
-          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(user + bot.reva.get(data.lang, "admin","ban5")));
+          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<:punish:836022893691011092> ${user} has been banned`));
           user.ban({ reason: args[1] });
       }
   }
