b63ee74387ae48d4f44046b7251563248adb7251
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 13:20:43 +0300

    Update vote.js

--- commands/general/vote.js
+++ commands/general/vote.js
@@ -9,13 +9,13 @@ module.exports = {
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
   ownerOnly: false,            
   cooldown: 2000,
-  run: async (bot, message, args, dev) => {
+  run: async (bot, message, args, dev, data) => {
    
  const embed = new Discord.MessageEmbed()
  .setColor(Color)
  .setThumbnail(`https://cdn.discordapp.com/attachments/818177981884923964/843887454348247100/image0.png`)
  .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
- .setTitle("Thank you for voting me")
+ .setTitle(bot.reva.get(data.lang, "general","vote"))
  .setDescription("[ Top.gg ](https://top.gg/bot/711328570374619207) \n - \n [ Discord.ly ](https://discord.ly/anti-vandalism)")
 
 message.channel.send(embed);
