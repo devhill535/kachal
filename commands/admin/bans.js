b44b110b464da55e55a7096ec1bb8b3c0a8e8c86
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 12:06:15 +0300

    Update bans.js

--- commands/admin/bans.js
+++ commands/admin/bans.js
@@ -12,12 +12,12 @@ module.exports = {
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS" ],        
   ownerOnly: false,            
   cooldown: 6000,
-  run: async (bot, message, args, dev) => {
+  run: async (bot, message, args, dev, data) => {
 
     if (!message.channel.guild) return;
     message.channel
     message.guild.fetchBans()
-      .then(bans => message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Server Ban List: **${bans.size}**`)))
+      .then(bans => message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(bot.reva.get(data.lang, "admin","bans") + bans.size)))
       .catch(console.error)
    } 
 }
