764191e16dbe81e7920596915dc4baabb39b3bf2
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 15:54:27 +0300

    Update language.js

--- commands/admin/language.js
+++ commands/admin/language.js
@@ -15,19 +15,19 @@ module.exports = {
   cooldown: 15000,
   run: async (client, message, args, dev) => {
       if (!args[1])
-        return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage : s!language [english/kurdish/arabic]`));
+        return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage : s!language [english/kurdish/arabic/turkish]`));
       let data = await Lang.findOne({ guildID: message.guild.id })
-      if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish" || args[1].toLowerCase() === "arabic") {
+      if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish" || args[1].toLowerCase() === "arabic" || args[1].toLowerCase() === "turkish") {
         data.language = args[1].toLowerCase();
         message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`
           Your server language is **${data.language}**`
         ));
       data.save();
       } else if (args[1] === "list") {
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Language list is :\n **english** ,**kurdish** ,**arabic**`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Language list is :\n **english** ,**kurdish** ,**arabic** ,**turkish**`));
       } else {
         message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`
-          Please Type\n \`s!lang english\` \n \`s!lang kurdish\` \n \`s!lang arabic\``
+          Please Type\n \`s!lang english\` \n \`s!lang kurdish\` \n \`s!lang arabic\` \n \`s!lang turkish\``
         ));
      }
   }
