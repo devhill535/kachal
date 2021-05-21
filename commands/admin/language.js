e0b218e4ee017aca24495b3cd25d378bdc52cd9f
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 04:56:40 +0300

    Update language.js

--- commands/admin/language.js
+++ commands/admin/language.js
@@ -12,22 +12,20 @@ module.exports = {
   cooldown: 15000,
   run: async (client, message, args, dev) => {
       if (!args[1])
-        return message.channel.send(
-          "**You need to set language**"
-        );
+        return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(``));
       let data = await Lang.findOne({ guildID: message.guild.id })
       if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish") {
         data.language = args[1].toLowerCase();
-        message.channel.send(
-          `** Your server language is \`${data.language}\`**`
-        );
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`
+          Your server language is \`${data.language}\``
+        ));
       data.save();
       } else if (args[1] === "list") {
-        message.channel.send("Language list is :\n `english`,`kurdish`")
-      } else{
-        message.channel.send(
-          `Usage : \n \<prefix>language <language>`
-        );
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Language list is :\n `english`,`kurdish``));
+      } else {
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`
+          Usage : s!language <language>`
+        ));
      }
   }
 }
