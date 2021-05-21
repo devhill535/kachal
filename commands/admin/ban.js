c1a1b38d22c2952ee69b219c6c29b42ac1d15ecb
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 12:53:56 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -11,7 +11,7 @@ const { Color } = require("../../config.js");
    botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","BAN_MEMBERS" ],		
    ownerOnly: false,			
    cooldown: 6000,
- run: async (bot, message, dev) => {
+ run: async (bot, message, dev, data) => {
    
  if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`You Don't have the permission **BAN_MEMBERS**`));
           let user = message.mentions.members.first();
@@ -21,7 +21,7 @@ const { Color } = require("../../config.js");
           if(!user || !args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage: s!ban [@User]`));
           if(message.mentions.users.size < 1) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Noy found this member`));
           if(!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`This member have role highst me can't ban`));
-          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<:punish:836022893691011092> ${user} has been banned`));
+          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(user + bot.reva.get(data.lang, "admin","bans")));
           user.ban({ reason: args[1] });
       }
   }
