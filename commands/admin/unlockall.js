d7c7c52580db8da959e6a2996bb8d518b8d07a06
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 04:05:22 +0300

    Update unlockall.js

--- commands/admin/unlockall.js
+++ commands/admin/unlockall.js
@@ -18,7 +18,7 @@ module.exports = {
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
       .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
-      .setDescription(.setDescription(bot.reva.get(data.lang, "admin","unlockall_message"));
+      setDescription(bot.reva.get(data.lang, "admin","unlockall_message"));
       message.channel.send(embed);
 
     message.guild.channels.cache.filter(c => c.name).forEach(async channel => {
