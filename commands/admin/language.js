88bdd3d135fa5933662c0871ed2ff71323afc5a6
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 13:35:33 +0300

    Update language.js

--- commands/admin/language.js
+++ commands/admin/language.js
@@ -15,7 +15,7 @@ module.exports = {
   cooldown: 15000,
   run: async (client, message, args, dev) => {
       if (!args[1])
-        return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`You need to set language`));
+        return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage : s!language [english/kurdish/arabic]`));
       let data = await Lang.findOne({ guildID: message.guild.id })
       if (args[1].toLowerCase() === "english" || args[1].toLowerCase() === "kurdish") {
         data.language = args[1].toLowerCase();
@@ -24,7 +24,7 @@ module.exports = {
         ));
       data.save();
       } else if (args[1] === "list") {
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Language list is :\n **english** ,**kurdish**`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Language list is :\n **english** ,**kurdish** ,**arabic**`));
       } else {
         message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`
           Usage : s!language <language>`
