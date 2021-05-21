8cc7dfd794e61357c8bbe817466c8d01bc0d4328
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 04:20:50 +0300

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -21,6 +21,7 @@ module.exports = {
 
         let embed = new Discord.MessageEmbed()
         .setColor(Color)
+        .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
         .setDescription(bot.reva.get(data.lang, "admin", "prefix_embed")   +   args[1])
         message.channel.send(embed);
         dataa.prefix = args[1];
