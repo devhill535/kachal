214c96a618887e836d1e2e1af7696010c9643faf
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-21 03:34:10 +0300

    Update lockall.js

--- commands/admin/lockall.js
+++ commands/admin/lockall.js
@@ -12,18 +12,13 @@ module.exports = {
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","MANAGE_CHANNELS" ],        
   ownerOnly: false,            
   cooldown: 6000,
-  run: async (bot, message, args, dev) => {
+  run: async (bot, message, args, dev, data) => {
     
-      let reason = args.slice(1).join(` `);
-      if(!reason) reason = 'Nothing..'
+   
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
       .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
-      .setDescription(`
-<:lock:828370866403803226> Locked all channels
-
-Reason
-${reason}`);
+      .setDescription(bot.reva.get(data.lang, "admin","lockall_message"));
       message.react("<a:true:836711495478804520>");
       message.channel.send(embed);
 
