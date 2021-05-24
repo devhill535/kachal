0ffbd4eab34af9e3fdf0ee5a9ba76beaa5f0f5b2
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-24 13:52:31 +0300

    Update userinfo.js

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -18,12 +18,13 @@ module.exports = {
  let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
    let flags = message.flags.toArray();
     if(message.flags.toArray() < 1) flags = "None";
-    
+    let nickname = member.nickname !== undefined && member.nickname !== null ? member.nickname : "None";
     const embed = new MessageEmbed()
       .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
       .setColor(Color)
       .setThumbnail(member.user.displayAvatarURL())
       .addField("Username", member.user.tag, true)
+      .addField("Nickname", `${nickname}`, true)
       .addField("User Id", `${member.id}`, true)
       .addField("Flags", `${flags}`, true)
       .addField("Joined At", member.joinedAt.toDateString())
