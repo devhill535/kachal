a0d06f9b15d32b5bd12d0046e202b2654490089e
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-26 11:33:16 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -12,31 +12,4 @@ module.exports = {
     ownerOnly: false,
     cooldown: 6000,
     run: async (client, args, message, dev) => {
-    let member = message.mentions.members.first();
-    if (!member)
-      return message.channel.send(`Please mention a user or provide a valid user ID`);
-    if (member === message.member)
-      return message.channel.send(`You cannot ban yourself`); 
-    if (member.roles.highest.position >= message.member.roles.highest.position)
-      return message.channel.send(`You cannot ban someone with an equal or higher role`);
-    if (!member.bannable)
-      return message.channel.send(`Provided member is not bannable`);
-
-    let reason = args.slice(1).join(' ');
-    if (!reason) reason = '`None`';
-    if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
-    
-    await member.ban({ reason: reason });
-
-    const embed = new MessageEmbed()
-      .setTitle('Ban Member')
-      .setDescription(`${member} was successfully banned.`)
-      .addField('Moderator', message.member, true)
-      .addField('Member', member, true)
-      .addField('Reason', reason)
-      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
-      .setTimestamp()
-      .setColor("");
-    message.channel.send(embed);
-    }
-}
+   
