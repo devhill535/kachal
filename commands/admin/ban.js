a68fe84da3a302cd06797895245dbeaad2dc7a50
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-26 11:33:40 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -13,3 +13,5 @@ module.exports = {
     cooldown: 6000,
     run: async (client, args, message, dev) => {
    
+if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`You Don't have the permission **BAN_MEMBERS**`));
+           let user = message.mentions.members.first();
