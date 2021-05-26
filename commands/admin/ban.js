dcff8456c54f7d4741cc40c727b427daa68a2b92
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-26 05:58:19 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -1,27 +1,42 @@
 const Discord = require('discord.js')
-const { Color } = require("../../config.js");
- module.exports = {
-   name: "ban",
-   aliases: ["band"],
-   description: "You can ban a member, or multiple members using this command",
-   usage: ["s!ban [@User]"],
-   category: ["Moderation"],
-   enabled: true,			  
-   memberPermissions: [ "BAN_MEMBERS" ],			
-   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","BAN_MEMBERS" ],		
-   ownerOnly: false,			
-   cooldown: 6000,
- run: async (bot, message, dev, data) => {
-   
- if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`You Don't have the permission **BAN_MEMBERS**`));
-          let user = message.mentions.members.first();
+const { MessagEmbed } = require("discord.js");
+module.exports = {
+    name: "ban",
+    aliases: ["band"],
+    description: "You can ban a member, or multiple members using this command",
+    usage: ["s!ban [@User]"],
+    category: ["Moderation"],
+    enabled: true,
+    memberPermissions: ["BAN_MEMBERS"],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
+    ownerOnly: false,
+    cooldown: 6000,
+    run: async (client, args, message, dev) => {
+    const member = this.getMemberFromMention(message, args[0]) || message.guild.members.cache.get(args[0]);
+    if (!member)
+      return this.sendErrorMessage(message, 0, 'Please mention a user or provide a valid user ID');
+    if (member === message.member)
+      return this.sendErrorMessage(message, 0, 'You cannot ban yourself'); 
+    if (member.roles.highest.position >= message.member.roles.highest.position)
+      return this.sendErrorMessage(message, 0, 'You cannot ban someone with an equal or higher role');
+    if (!member.bannable)
+      return this.sendErrorMessage(message, 0, 'Provided member is not bannable');
 
+    let reason = args.slice(1).join(' ');
+    if (!reason) reason = '`None`';
+    if (reason.length > 1024) reason = reason.slice(0, 1021) + '...';
+    
+    await member.ban({ reason: reason });
 
-          let args = message.content.split(' ');
-          if(!user || !args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Usage: s!ban [@User]`));
-          if(message.mentions.users.size < 1) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`Noy found this member`));
-          if(!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`This member have role highst me can't ban`));
-          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<:punish:836022893691011092> ${user} has been banned`));
-          user.ban({ reason: args[1] });
-      }
-  }
+    const embed = new MessageEmbed()
+      .setTitle('Ban Member')
+      .setDescription(`${member} was successfully banned.`)
+      .addField('Moderator', message.member, true)
+      .addField('Member', member, true)
+      .addField('Reason', reason)
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor("");
+    message.channel.send(embed);
+    }
+}
