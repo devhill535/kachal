d04a9de70bf8c0a048a4ca792be6a2419bbe0b44
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-30 00:21:43 +0300

    Update staff.js

--- commands/general/staff.js
+++ commands/general/staff.js
@@ -10,12 +10,12 @@ module.exports = {
   cooldown: 10000,
   run: async (bot, message, args, dev) => {
     const guild = await message.guild.fetch();
-    const administrators = guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
-    const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
+    const administrators = guild.members.cache.filter((m) => message.hasPermission("ADMINISTRATOR") && !bot.user.bot);
+    const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && message.hasPermission("MANAGE_MESSAGES") && !message.user.bot);
     const embed = new Discord.MessageEmbed()
       .setAuthor("")
-      .addField("administrators.size > 0 ? administrators.map((a) =>", `${this.client.customEmojis.status[a.presence.status]} | ${a.user.tag}`).join("\n")
-      .addField("moderators.size > 0 ? moderators.map((m) =>",  `${this.client.customEmojis.status[m.presence.status]} | ${m.user.tag}`).join("\n")
+      .addField("administrators.size > 0 ? administrators.map((a) =>", `| ${bot.user.tag}`).join("\n")
+      .addField("moderators.size > 0 ? moderators.map((m) =>",  `| ${message.user.tag}`).join("\n")
       .setColor("")
       .setFooter("");
     message.channel.send(embed);
