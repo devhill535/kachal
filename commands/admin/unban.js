fa67f23295c8acfc15f4be8d1a8d361ae2d03bb7
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-30 05:36:02 +0300

    Update unban.js

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -1,1 +1,59 @@
+    if (!message.member.hasPermission("BAN_MEMBERS"))
+      return message.channel.send(
+        `You Don't Have Permission To Use This Command!`
+      );
 
+    if (!args[0])
+      return message.channel.send(
+        `Please Give Me Member ID That You Want To Unban!`
+      );
+
+    if (isNaN(args[0])) return message.channel.send(`Please Give Me Valid ID!`);
+
+    if (args[0] === message.author.id)
+      return message.channel.send(`You Are Already Unban!`);
+
+    if (args[0] === message.guild.owner.user.id)
+      return message.channel.send(`Server Owner Is Already Unban!`);
+
+    if (args[0] === client.user.id)
+      return message.channel.send(`I Am Already Unban!`);
+
+    let FetchBan = await message.guild.fetchBans();
+
+    let Member;
+    Member =
+      FetchBan.find(
+        b => b.user.username.toLowerCase() === args[0].toLocaleLowerCase()
+      ) ||
+      FetchBan.get(args[0]) ||
+      FetchBan.find(
+        bm => bm.user.tag.toLowerCase() === args[0].toLocaleLowerCase()
+      );
+
+    if (!Member)
+      return message.channel.send(
+        "Please Give Valid Member ID Or Member Is Not Banned!"
+      );
+
+    let Reason = args.slice(1).join(" ") || "With out reason";
+
+    try {
+      message.guild.members.unban(Member.user.id, Reason);
+    } catch (error) {
+      return message.channel.send(
+        `I Can't Unban That Member Maybe Member Is Not Banned Or Some Error!`
+      );
+    }
+
+    let embed = new MessageEmbed()
+      .setColor("BLUE")
+     
+      .setDescription(`<@${Member.user.id}> Unbanned from server`)
+    
+
+    return message.channel.send(embed);
+
+   
+  }
+}
