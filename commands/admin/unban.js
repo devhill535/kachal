43ccd68fb960a083a44a0dbb6d4b839d629d92f8
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-30 05:36:42 +0300

    Update unban.js

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -1,4 +1,18 @@
-    if (!message.member.hasPermission("BAN_MEMBERS"))
+ const Discord = require('discord.js')
+const { MessagEmbed } = require("discord.js");
+module.exports = {
+    name: "unban",
+    aliases: ["unband"],
+    description: "You can ban a member, or multiple members using this command",
+    usage: ["s!ban [@User]"],
+    category: ["Moderation"],
+    enabled: true,
+    memberPermissions: ["BAN_MEMBERS"],
+    botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
+    ownerOnly: false,
+    cooldown: 6000,
+    run: async (bot, arge, message, dev) => {
+  if (!message.member.hasPermission("BAN_MEMBERS"))
       return message.channel.send(
         `You Don't Have Permission To Use This Command!`
       );
