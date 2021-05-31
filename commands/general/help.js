c406969c909d088aacb25e3e54ad9bb531203670
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 14:59:58 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -16,8 +16,7 @@ module.exports = {
    
     if (!args[1]) {
   let embed = new Discord.MessageEmbed()
-   .setColor(Color)
-     .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
+     .setColor(Color)
      .setTitle(bot.reva.get(data.lang, "general","help_embed"))
      .setDescription(`
 [ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/SCvu2jCF7v) - [ Website ](https://zalmanti25.wixsite.com/my-site)
@@ -37,8 +36,7 @@ module.exports = {
         ccmd = "<:enable:840230134899671060> Enabled"
         }
       let embed = new Discord.MessageEmbed()
-      .setColor("BLUE") 
-      .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
+      .setColor(Color) 
       .setThumbnail(message.author.avatarURL())
       .setTitle("**Help**")
       .setDescription(command.description || command.name + " this command don't have a description")
