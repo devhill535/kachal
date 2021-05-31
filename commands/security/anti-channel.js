058e52ec70ea471a2f3e3ce6417446ab03e98685
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 16:58:39 +0300

    Update anti-channel.js

--- commands/security/anti-channel.js
+++ commands/security/anti-channel.js
@@ -22,7 +22,6 @@ module.exports = {
       guild.save();
       const embed = new Discord.MessageEmbed()
         .setColor(Color)
-        .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
         .setDescription(`<a:true:836711495478804520> Anti-Channel Is **Enabled**`);
       return message.channel.send(embed);
      } else if (args[1] === "off") {
@@ -30,14 +29,12 @@ module.exports = {
          guild.save();
       const embed1 = new Discord.MessageEmbed()
         .setColor(Color)
-        .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
         .setDescription(`<a:false:836711508246659109> Anti-Channel Is **Disabled**`);
       return message.channel.send(embed1);
     }
     if (isNaN(num) || parseInt(num) < 1) {
       const embed2 = new Discord.MessageEmbed()
         .setColor(Color)
-        .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
         .setTitle("<a:false:836711508246659109> Invalid Usage")
         .setDescription(`Please Type\n \`s!antichannel on\` \n \`s!antichannel off\` \n \`s!antichannel\` **<number>**`
         );
@@ -47,7 +44,6 @@ module.exports = {
     guild.save();
     const embed3 = new Discord.MessageEmbed()
       .setColor(Color)
-      .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
       .setDescription(`Successfully changed to ${guild.channel.lmite} <:punish:836022893691011092>
 `);
     return message.channel.send(embed3);
