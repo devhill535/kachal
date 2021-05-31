e0bf3a1a9d66f93b6031fef97fac0cf7107d98b8
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 17:14:13 +0300

    Update anti-role.js

--- commands/security/anti-role.js
+++ commands/security/anti-role.js
@@ -36,8 +36,7 @@ module.exports = {
     if (isNaN(num) || parseInt(num) < 1) {
       const embed2 = new Discord.MessageEmbed()
         .setColor(Color)
-        .setTitle("<a:false:836711508246659109> Invalid Usage")
-        .setDescription(`Please Type\n \`s!antirole on\` \n \`s!antirole off\` \n \`s!antirole\` **<number>**`
+        .setDescription(`error syntax <a:false:836711508246659109>\n ${guild.prefix}antirole [\`on\`,\`off\`,\`<number>\`]`
         );
       return message.channel.send(embed2);
     }
