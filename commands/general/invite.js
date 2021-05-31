dcc377d937c06fa55914baa0995ff73fb5aa7a10
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 15:18:39 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -14,16 +14,11 @@ module.exports = {
   ownerOnly: false,
   cooldown: 2000,
   run: async (bot, message, args, dev, data) => {
-    const inviteLink = bot.config.inviteURL || `https://discordapp.com/oauth2/authorize?client_id=${this.bot.user.id}&scope=bot&permissions=2146958847`;
-
-		if(args[0] && args[0] === "copy"){
-			return message.channel.send(inviteLink);
-		
+    
 const embed = new Discord.MessageEmbed()
  .setColor(Color)
  .setTitle(bot.reva.get(data.lang, "general","invite"))
  .setDescription(`[Anti Vandalism](https://discord.com/api/oauth2/authorize?client_id=711328570374619207&permissions=8&scope=bot)`)
-.addField("Link bot", inviteLink)
 
 message.channel.send(embed);
     }
