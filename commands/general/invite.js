8de26e5b996aee66dfa717f62a7da904502b69dc
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 15:01:33 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -14,22 +14,22 @@ module.exports = {
   ownerOnly: false,
   cooldown: 2000,
   run: async (bot, message, args, dev, data) => {
-    
-const embed = new Discord.MessageEmbed()
- .setColor(Color)
- .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
- .setTitle(bot.reva.get(data.lang, "general","invite"))
- .setDescription(`[Anti Vandalism](https://discord.com/api/oauth2/authorize?client_id=711328570374619207&permissions=8&scope=bot)`)
-message.channel.send(embed);
-    }
-}
-               /* const inviteLink =  || `https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2146958847`;
+    const inviteLink =  || `https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=2146958847`;
 
 		if(args[0] && args[0] === "copy"){
 			return message.channel.send(inviteLink);
 		}
         
-		const embed = new Discord.MessageEmbed()
-			
-			.addField("Link bot", inviteLink)
-			*/
+const embed = new Discord.MessageEmbed()
+ .setColor(Color)
+ .setTitle(bot.reva.get(data.lang, "general","invite"))
+ .setDescription(`[Anti Vandalism](https://discord.com/api/oauth2/authorize?client_id=711328570374619207&permissions=8&scope=bot)`)
+.addField("Link bot", inviteLink)
+
+message.channel.send(embed);
+    }
+}
+              
+		
+		
+		
