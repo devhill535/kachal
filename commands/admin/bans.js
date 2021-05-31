3fa85d7efeec20e5faab58f154d98c5fa7599541
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 14:51:33 +0300

    Update bans.js

--- commands/admin/bans.js
+++ commands/admin/bans.js
@@ -17,7 +17,7 @@ module.exports = {
     if (!message.channel.guild) return;
     message.channel
     message.guild.fetchBans()
-      .then(bans => message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(bot.reva.get(data.lang, "admin","bans") + bans.size)))
+      .then(bans => message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(bot.reva.get(data.lang, "admin","bans") + bans.size)))
       .catch(console.error)
    } 
 }
