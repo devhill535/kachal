d4bb64e746e7f08fff09872836a6ae09649ce10b
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 14:56:38 +0300

    Update prefix.js

--- commands/admin/prefix.js
+++ commands/admin/prefix.js
@@ -14,14 +14,13 @@ module.exports = {
   ownerOnly: false,            
   cooldown: 10000,
   run: async (bot, message, args, dev, data) => {
-        if(!args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(bot.reva.get(data.lang, "admin","prefix_type")));
-        if(args[1].length > 5) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(bot.reva.get(data.lang, "admin","prefix_length")));
+        if(!args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(bot.reva.get(data.lang, "admin","prefix_type")));
+        if(args[1].length > 5) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(bot.reva.get(data.lang, "admin","prefix_length")));
          
         let dataa = await Guild.findOne({ guildID: message.guild.id })
 
         let embed = new Discord.MessageEmbed()
         .setColor(Color)
-        .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
         .setDescription(bot.reva.get(data.lang, "admin", "prefix_embed") + args[1])
         message.channel.send(embed);
         dataa.prefix = args[1];
