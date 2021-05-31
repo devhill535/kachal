59f4eaf2177e18b3c841e0889e561a20985ee9c5
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-31 14:54:59 +0300

    Update lockall.js

--- commands/admin/lockall.js
+++ commands/admin/lockall.js
@@ -17,7 +17,6 @@ module.exports = {
    
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
       .setDescription(bot.reva.get(data.lang, "admin","lockall_message"));
       message.channel.send(embed);
 
