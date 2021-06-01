4af52c549292cc03148c888ca489773200c6f0ae
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-01 18:07:46 +0300

    Update wordWhitelist.js

--- commands/owner/wordWhitelist.js
+++ commands/owner/wordWhitelist.js
@@ -22,7 +22,7 @@ module.exports = {
           message.guild.members.cache.get(args[2]) ||
           message.mentions.members.first();
         if (!user)
-          return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(
+          return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(
             `<@${message.author.id}> mention someone`
           ));
         if(!data.worldWhitelist.find((c) => c.type === user.id)){
@@ -37,16 +37,16 @@ module.exports = {
             }
          },
         })     
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<a:true:836711495478804520> ${user.user.username} Added to developer`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`<a:true:836711495478804520> ${user.user.username} Added to developer`));
           } else {
-          message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`this man is whitelisted`));
+          message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`this man is whitelisted`));
           }
       } else if (args[1] === "remove") {
         let user =
           message.guild.members.cache.get(args[2]) ||
           message.mentions.members.first();
         if (!user)
-          return message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(
+          return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(
             `<@${message.author.id}> mention someone`
           ));
         if(data.worldWhitelist.find((c) => c.type === user.id)){
@@ -61,18 +61,17 @@ module.exports = {
             }
          },
         })
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<a:false:836711508246659109> ${user.user.username} Removed in developer`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`<a:false:836711508246659109> ${user.user.username} Removed in developer`));
         } else {
-        message.channel.send(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`<a:false:836711508246659109> ${user.user.username} Not in developer`));
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`<a:false:836711508246659109> ${user.user.username} Not in developer`));
         };
       } else if (!args[1]) {
-        if (data.worldWhitelist.length === 0) return message.reply(new Discord.MessageEmbed().setColor(Color).setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif").setDescription(`No one developer!`));
+        if (data.worldWhitelist.length === 0) return message.reply(new Discord.MessageEmbed().setColor(Color).setDescription(`No one developer!`));
        let arrayOfCustomCommands = data.worldWhitelist.map(w => `⇰ <@${w.type}>  - ${w.type}`)
         
         let embed = new Discord.MessageEmbed()
       .setTitle("Developer Bot")
       .setColor(Color)
-      .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
       .setDescription(arrayOfCustomCommands.slice(0, 15).join('\n'));
       message.channel.send(embed);
       }
