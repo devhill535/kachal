07f3f4a4e1754ee6b034d0582277476c3d9ba685
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-01 20:21:44 +0300

    Update antirole.js

--- data/antirole.js
+++ data/antirole.js
@@ -1,6 +1,6 @@
 schema = mongoose.Schema({	
 	guildID: String,
-	rStatus: { onoff: { type: String, default: "off" },
+	rStatus: { onoff: { type: String, default: "on" },
 	}
 	});
 	module.exports = mongoose.model("AntiRole", schema)
