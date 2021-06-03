4d67346e7072186f554586d3aa7f3626a9da0280
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 01:50:32 +0300

    Update staff.js

--- commands/general/staff.js
+++ commands/general/staff.js
@@ -1,3 +1,16 @@
+const Discord = require("discord.js")
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "vote",
+  aliases: ["v"],
+  enabled: true,            
+  memberPermissions: [ "SEND_MESSAGES" ],            
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
+  ownerOnly: false,            
+  cooldown: 2000,
+  run: async (bot, message, args, dev, data) => {
+
 const guild = await message.guild.fetch();
 		const administrators = guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
 		const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
@@ -5,7 +18,7 @@ const guild = await message.guild.fetch();
 		
 			.addField(message.translate("general/staff:ADMINS"), (administrators.size > 0 ? administrators.map((a) => `${this.client.customEmojis.status[a.presence.status]} | ${a.user.tag}`).join("\n") :message.translate("general/staff:NO_ADMINS")))
 			.addField(message.translate("general/staff:MODS"), (moderators.size > 0 ? moderators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${m.user.tag}`).join("\n") : message.translate("general/staff:NO_MODS")))
-			.setColor("")
+			.setColor(Color)
 		
 		message.channel.send(embed);
 	}
