f334a65bdd67e56567add4c5eb06ea2da7515426
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 00:32:34 +0300

    Delete publicrole.js

--- data/publicrole.js
+++
@@ -1,9 +0,0 @@
-schema = mongoose.Schema({
-  guildID: String,
-  pRoles: { type: Array, default: [] },
-  
-  pStatus: { onoff : { type: String, default:  "off" }
-  },
-  
-});
-module.exports = mongoose.model("PublicRole", schema)
