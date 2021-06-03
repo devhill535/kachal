27b3aabca9cb007b2af1267ea76d232c38156bcb
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 00:30:55 +0300

    Update anti-roleUpdate.js

--- commands/security/anti-roleUpdate.js
+++ commands/security/anti-roleUpdate.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
   name: ["antiroleupdate"],
   aliases: ["anti-roleupdate"],
-  enabled: false,
+  enabled: true,
   memberPermissions: ["SEND_MESSAGES"],
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   ownerOnly: false,
