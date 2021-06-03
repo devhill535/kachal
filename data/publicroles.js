595690def0a4573499e6fa7d7b4933fd58a15c58
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 00:23:53 +0300

    Create publicroles.js

---
+++ data/publicroles.js
@@ -0,0 +1,9 @@
+schema = mongoose.Schema({
+  guildID: String,
+  pRoles: { type: Array, default: [] },
+  
+  pStatus: { onoff : { type: String, default:  "off" }
+  },
+  
+});
+module.exports = mongoose.model("PublicRoles", schema)
