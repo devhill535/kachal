d5b23fbbda0d1dca889fd5380d8c9abfbd04e3f0
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 23:23:44 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,18 +1,17 @@
 const Discord = require("discord.js")
-const { Color } = require("../../config.js");
 
 module.exports = class {
 async run(guild, bot) {
 		const thanksEmbed = new Discord.MessageEmbed()			
                         .setAuthor("Thank you for adding me to your guild !")
 			.setDescription("To configure me,\n type **s!help** and look at the **Security** commands!.")
-			.setColor(Color)
+			.setColor("#2c2f33")
 		guild.owner.send(thanksEmbed).catch(() => {});
     
 	
 		const text = "<a:true:836711495478804520> **__Joined New Guild__** \n **Guild Name**: "+guild.name+" \n **Guild Owner Name**: " + `${guild.owner.user.username}` + " \n **Guild Owner ID**: " + `${guild.owner.id}` + "\n **Guild Member Size**: "+guild.memberCount+" \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
 		const logsEmbed = new Discord.MessageEmbed()
-			.setColor(Color)
+			.setColor("#2c2f33")
 			.setDescription(text);
 		bot.channels.cache.get("850465538306998272").send(logsEmbed);     
 }};
