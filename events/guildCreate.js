1e6a5cec2900abceb8921918562dbdf8dfa9d8b4
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 23:09:09 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,15 +3,15 @@ module.exports = class {
 async run(guild, bot) {
 		const thanksEmbed = new Discord.MessageEmbed()			
                         .setAuthor("Thank you for adding me to your guild !")
-			.setDescription("To configure me,\n type **s!help** and look at the administration commands!.")
-			.setColor("BLACK")
-			.setTimestamp();
+			.setDescription("To configure me,\n type **s!help** and look at the **Security** commands!.")
+			.setColor(Color)
 		guild.owner.send(thanksEmbed).catch(() => {});
     
 	
-		const text = "✅ **__Joined New Guild__** \n **Guild Name** = "+guild.name+" \n **Guild Owner Name** = " + `${guild.owner.user.username}` + " \n **Guild Owner ID** = " + `${guild.owner.id}` + "\n **Guild Member Size** = "+guild.memberCount+" \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
+		const text = "✅ **__Joined New Guild__** \n **Guild Name**: "+guild.name+" \n **Guild Owner Name**: " + `${guild.owner.user.username}` + " \n **Guild Owner ID**: " + `${guild.owner.id}` + "\n **Guild Member Size**: "+guild.memberCount+" \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
 		const logsEmbed = new Discord.MessageEmbed()
-			.setColor("BLACK")
+			.setColor(Color)
 			.setDescription(text);
+                        .setTimestamp();
 		bot.channels.cache.get("835968578699264011").send(logsEmbed);     
 }};
