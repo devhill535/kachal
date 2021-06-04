2eab1580ca74f36eb0e3b0bba6f2a06f4fd92173
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 23:24:07 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,18 +1,17 @@
 const Discord = require("discord.js")
-const { Color } = require("../../config.js");
 
 module.exports = class {
 async run(guild, bot) {
 		const thanksEmbed = new Discord.MessageEmbed()			
                        .setAuthor("I'm leaving in your server !!")
 			.setDescription("If you have a report join this server https://discord.gg/5RsPXCwG8c")
-			.setColor(Color)
+			.setColor("#2c2f33")
 		guild.owner.send(thanksEmbed).catch(() => {});
     
 	
 		const text = "<a:false:836711508246659109> **__Leaving Old Guild__** \n **Guild Name**: "+guild.name+" \n **Guild Owner Name**: " + `${guild.owner.user.username}` + " \n **Guild Owner ID**: " + `${guild.owner.id}` + " \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
 		const logsEmbed = new Discord.MessageEmbed()
-			.setColor(Color)
+			.setColor("#2c2f33")
 			.setDescription(text);
 		bot.channels.cache.get("850465538306998272").send(logsEmbed);     
 }};
