b82a2ef82360283865fd752c7e6309a338844489
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 22:42:07 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -13,70 +13,44 @@ module.exports = {
     cooldown: 6000,
     run: async (client, args, message, dev) => {
    
-    //Start
-   
-    if (!message.member.hasPermission("BAN_MEMBERS"))
-      return message.channel.send(
-        `You Don't Have Permission To Use This Command!`
-      );
+  if(message.member.hasPermission('BAN_MEMBERS')){
+            let reason = args.slice(1).join(' ')
 
-    let Member = message.mentions.users.first();
+            const user = message.mentions.members.first()
+            if(!user){
+                const embed = new MessageEmbed()
+                .setDescription(`<:emojino:796264735649300480> Please mention a member to ban!`)
+                .setColor('ORANGE')
+                .setFooter('Hope\'s Discord Cafe')
+                .setTimestamp()
+                message.channel.send(embed)
+            }
 
-    if (!Member)
-      return message.channel.send(
-        `Please Mention A Member That You Want To Ban!`
-      );
-
-    if (!message.guild.members.cache.get(Member.id))
-      return message.channel.send(`Please Mention A Valid Member!`);
-
-    if (Member.id === message.author.id)
-      return message.channel.send(`You Can't Ban Your Self!`);
-
-    if (Member.id === client.user.id)
-      return message.channel.send(`Please Don't Ban Me ;-;`);
-
-    if (Member.id === message.guild.owner.user.id)
-      return message.channel.send(`You Can't Ban Owner Of Server!`);
-
-    let Reason = args.slice(1).join(" ");
-
-    let User = message.guild.member(Member);
-
-    if (!User.bannable) return message.channel.send(`I Can't Ban That Member!`);
-
-    try {
-      console.log(`Member Is Going To Get Ban!`);
-      setTimeout(function() {
-        User.ban({ reason: `${Reason || "No Reason Provided!"}` });
-      }, 2000);
-      let embed = new Discord.MessageEmbed()
-        .setColor("")
-        .setTitle(`Command : Ban ✈️`)
-        .addField(`**Banned Member:**`, `<@${Member.id}>`)
-        .addField(`**Member Banned:**`, `<@${message.author.id}>`)
-        .addField(`**Reason:**`, `${Reason || "There was no reason!"}`)
-        .setFooter(`${message.author.username}`)
-        .setTimestamp();
-      if (User && Member.bot === false)
-        Member.send(
-          `You Have Been Banned From **${message.guild.name}** For ${Reason ||
-            "There was no reason!"}`
-        );
-      message.channel.send(embed);
-      console.log(
-        `User: ${Member.tag} Just Got Banned From ${
-          message.guild.name
-        } For ${Reason || "There was no reason!"}`
-      );
-    } catch (error) {
-      return message.channel
-        .send(
-          `I Can't Ban That Member Maybe Member Has Higher Role Than Me & My Role Is Lower Than Member!`
-        )
-        .then(() => console.log(error));
+            if(!reason) reason = 'No Reason Specified!'
+            
+            if(user.hasPermission('MANAGE_GUILD')){
+                const embed = new MessageEmbed()
+                .setDescription(`<:emojino:796264735649300480> That user is a mod/admin! I can't ban them!`)
+                .setColor('ORANGE')
+                .setFooter('Hope\'s Discord Cafe')
+                .setTimestamp()
+                message.channel.send(embed)
+            } else {
+                message.guild.members.ban(user.id, {days: 7, reason: reason})
+                const embed = new MessageEmbed()
+                .setDescription(`<:emojiyes:796264735833980968> Successfully banned <@${user.id}>!`)
+                .setColor('ORANGE')
+                .setFooter('Hope\'s Discord Cafe')
+                .setTimestamp()
+                message.channel.send(embed)
+            }
+        } else {
+            const embed = new MessageEmbed()
+            .setDescription(`<:emojino:796264735649300480> You don't have permissions to use this command!`)
+            .setColor('ORANGE')
+            .setFooter('Hope\'s Discord Cafe')
+            .setTimestamp()
+            message.channel.send(embed)
+        }
     }
-
-    //End
-  }
-};
+}
