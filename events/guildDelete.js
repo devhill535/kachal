fe152f5820ef7e9c4e26bb7caf432c0d7c9b893d
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 23:11:06 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,18 +1,17 @@
-
 const Discord = require("discord.js")
 module.exports = class {
 async run(guild, bot) {
 		const thanksEmbed = new Discord.MessageEmbed()			
                        .setAuthor("I'm leaving in your server !!")
-			.setDescription("If you have a report join this server https://discord.gg/c9KfdcTffn ")
-			.setColor("BLACK")
-			.setTimestamp();
+			.setDescription("If you have a report join this server https://discord.gg/5RsPXCwG8c")
+			.setColor(Color)
 		guild.owner.send(thanksEmbed).catch(() => {});
     
 	
-		const text = "❌ **__Leaving Old Guild__** \n **Guild Name** = "+guild.name+" \n **Guild Owner Name** = " + `${guild.owner.user.username}` + " \n **Guild Owner ID** = " + `${guild.owner.id}` + " \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
+		const text = "❌ **__Leaving Old Guild__** \n **Guild Name**: "+guild.name+" \n **Guild Owner Name**: " + `${guild.owner.user.username}` + " \n **Guild Owner ID**: " + `${guild.owner.id}` + " \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
 		const logsEmbed = new Discord.MessageEmbed()
-			.setColor("BLACK")
+			.setColor(Color)
 			.setDescription(text);
+                        .setTimestamp();
 		bot.channels.cache.get("835968578699264011").send(logsEmbed);     
 }};
