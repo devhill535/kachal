7b2a2b0406996493cb71c9b92f98fcb4584c229c
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-04 22:44:54 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -13,44 +13,19 @@ module.exports = {
     cooldown: 6000,
     run: async (client, args, message, dev) => {
    
-  if(message.member.hasPermission('BAN_MEMBERS')){
-            let reason = args.slice(1).join(' ')
-
-            const user = message.mentions.members.first()
-            if(!user){
-                const embed = new MessageEmbed()
-                .setDescription(`<:emojino:796264735649300480> Please mention a member to ban!`)
-                .setColor('ORANGE')
-                .setFooter('Hope\'s Discord Cafe')
-                .setTimestamp()
-                message.channel.send(embed)
-            }
-
-            if(!reason) reason = 'No Reason Specified!'
-            
-            if(user.hasPermission('MANAGE_GUILD')){
-                const embed = new MessageEmbed()
-                .setDescription(`<:emojino:796264735649300480> That user is a mod/admin! I can't ban them!`)
-                .setColor('ORANGE')
-                .setFooter('Hope\'s Discord Cafe')
-                .setTimestamp()
-                message.channel.send(embed)
-            } else {
-                message.guild.members.ban(user.id, {days: 7, reason: reason})
-                const embed = new MessageEmbed()
-                .setDescription(`<:emojiyes:796264735833980968> Successfully banned <@${user.id}>!`)
-                .setColor('ORANGE')
-                .setFooter('Hope\'s Discord Cafe')
-                .setTimestamp()
-                message.channel.send(embed)
-            }
-        } else {
-            const embed = new MessageEmbed()
-            .setDescription(`<:emojino:796264735649300480> You don't have permissions to use this command!`)
-            .setColor('ORANGE')
-            .setFooter('Hope\'s Discord Cafe')
-            .setTimestamp()
-            message.channel.send(embed)
-        }
-    }
+      const mention = message.mentions.users.first()
+    const reason = args.slice(1).join(' ')
+    if (!mention) return message.reply('Mention a user.')
+    if (!reason) return message.reply('No Reason specified.')
+    if (
+      message.member.roles.highest.position <=
+      message.guild.member(mention).roles.highest.position
+    )
+      return message.reply('You cannot ban this user.')
+    message.guild.members.ban(mention.id, {
+      days: 7,
+      reason: `Banned by: ${message.author.id} | Reason: ${reason}`,
+    })
+    message.channel.send(`<@${mention.id}> has been successfully banned.`)
+  }
 }
