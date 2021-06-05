93e03e9288550633db8c8fdd011f3996b340771f
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-05 15:11:47 +0300

    Update ban.js

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -12,15 +12,42 @@ module.exports = {
     botPermissions: ["SEND_MESSAGES", "EMBED_LINKS", "BAN_MEMBERS"],
     ownerOnly: false,
     cooldown: 6000,
-    run: async (bot, message, dev) => {
-   
-  if(!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`You Don't have the permission **BAN_MEMBERS**`));
-            let user = message.mentions.members.first();
-            let args = message.content.split(' ');
-            if(!user || !args[1]) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Usage: s!ban [@User]`));
-            if(message.mentions.users.size < 1) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Noy found this member`));
-            if(!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`This member have role highst me can't ban`));
-            message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`<:punish:836022893691011092> ${user} has been banned`));
-            user.ban({ reason: args[1] });
-        }
-    }
+    run: async (client, message, args, dev) => {
+  
+     let user = message.mentions.members.first()
+
+    let perms = message.member.hasPermission("BAN_MEMBERS");
+    if (!perms) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`You don't have `BAN_MEMBERS` permission`));
+
+    if (!message.guild.me.permissions.has('BAN_MEMBERS'))
+      return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`"I need `BAN_MEMBERS` permission!`));
+
+    if (!user)
+      return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Usage: s!ban [@User]`)).catch(console.error);
+
+    if (user.id === message.author.id) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`You can't ban yourself`));
+
+    if (user.id === client.user.id) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`I can't ban myself`));
+
+    if (message.guild.ownerID !== message.author.id && user.roles.highest.comparePositionTo(message.member.roles.highest) >= 0)
+      return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`Based on the role hierarchy, you cannot ban this user`));
+
+    if (razon.length > 100) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription('The reason cannot exceed 100 characters'));
+
+    if (!message.guild.member(user).bannable) return message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`I cannot ban the mentioned user`));
+
+  
+    const embedBan = new Discord.MessageEmbed()
+      .setColor(Color)
+      .setDescription(`${user} has been banned`)
+
+    message.channel.send(embedBan);
+
+    const embedUserBan = new Discord.MessageEmbed()
+      .setColor(Color)
+      .setDescription(`${user} has been banned from **${message.guild.name}** server`)
+
+    user.send(embedUserBan).catch(e => message.channel.send(''))
+
+  }
+}
