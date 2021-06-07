975a07871eecf7ce45ab4d7496b76b9935b620f3
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-07 07:16:33 +0300

    Update antighistping.js

--- commands/admin/antighistping.js
+++ commands/admin/antighistping.js
@@ -11,17 +11,17 @@ module.exports = {
   botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
   ownerOnly: false,
   cooldown: 15000,
-  run: async (client, message, args, dev) => {
+  run: async (bot, message, args, dev) => {
 if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, you don\'t have the correct permissions to do that! `(MANAGE MESSAGES)`')
         if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, I don\'t have the correct permissions to do that! `(MANAGE MESSAGES)`')
-        if (args = ['on', 'On', 'True', 'true', 'TRUE', 'ON', 'activate', 'Activate', 'ACTIVATE']) {
+        if (args = ['on']) {
             if (await db.has(`ghost-${message.guild.id}`) === false) {
 
                 await db.set(`ghost-${message.guild.id}`, true),
                     message.channel.send('AntiGhostPing has been activated!')
             }
         }
-        if (args = ['off', 'Off', 'OFF', 'deactivate', 'Deactivate', 'DEACTIVATE']) {
+        if (args = ['off']) {
             if (await db.has(`ghost-${message.guild.id}`) === true) {
 
                 await db.delete(`ghost-${message.guild.id}`)
