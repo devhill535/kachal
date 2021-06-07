b4ebdd9ae33ba2c04a0a9a54d7d20e45ba290efd
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-07 18:10:44 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -21,9 +21,9 @@ module.exports = {
      .setDescription(`
 [ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)
 `)
-     .addField("ℹ️ General", "`invite`, `support`, `about`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`")
-     .addField("⚙️ Moderation", "`kick`, `ban`, `lock`, `unlock`, `lockall`, `unlockall`, `prefix`, `clear`, `unbanall`, `mute`, `lang`")
-     .addField("🔨 Security", "`anti`, `settings`, `punishment`, `whitelist`")
+     .addField("General Commands [8]", "`invite`, `support`, `about`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`")
+     .addField("Moderation Commands [11]", "`kick`, `ban`, `lock`, `unlock`, `lockall`, `unlockall`, `prefix`, `clear`, `unbanall`, `mute`, `lang`")
+     .addField("Security Commmands [4]", "`anti`, `settings`, `punishment`, `whitelist`")
   message.channel.send(embed)
        } else {
       let  command = args[1]
