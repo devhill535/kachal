1a1004dc42ef33f08dafa3453bc8f737e8af5c03
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-07 07:07:27 +0300

    Create antighistping.js

---
+++ commands/admin/antighistping.js
@@ -0,0 +1,19 @@
+if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, you don\'t have the correct permissions to do that! `(MANAGE MESSAGES)`')
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, I don\'t have the correct permissions to do that! `(MANAGE MESSAGES)`')
+        if (args = ['on', 'On', 'True', 'true', 'TRUE', 'ON', 'activate', 'Activate', 'ACTIVATE']) {
+            if (await db.has(`ghost-${message.guild.id}`) === false) {
+
+                await db.set(`ghost-${message.guild.id}`, true),
+                    message.channel.send('AntiGhostPing has been activated!')
+            }
+        }
+        if (args = ['off', 'Off', 'OFF', 'deactivate', 'Deactivate', 'DEACTIVATE']) {
+            if (await db.has(`ghost-${message.guild.id}`) === true) {
+
+                await db.delete(`ghost-${message.guild.id}`)
+                message.channel.send('AntiGhostPing has been deactivated!')
+            }
+
+        }
+    }
+}
