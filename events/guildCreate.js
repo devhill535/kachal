38422c1610fed9dcf909848f455bb53f52898ea4
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-10 21:14:11 +0300

    Delete guildCreate.js

--- events/guildCreate.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js")
-module.exports = class {
-async run(guild, bot) {
-		const thanksEmbed = new Discord.MessageEmbed()			
-                        .setAuthor("Thank you for adding me to your guild !")
-			.setDescription("To configure me,\n type **s!help** and look at the **Security** commands!.")
-			.setColor("#2c2f33")
-                        .setTimestamp();
-		guild.owner.send(thanksEmbed);
