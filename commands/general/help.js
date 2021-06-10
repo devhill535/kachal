ceb03bcc3ddd322f4df7f8193b690625a202fab7
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-10 21:58:44 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -21,7 +21,7 @@ module.exports = {
      .setDescription(`
 [ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)
 `)
-     .addField("General Commands [8]", "`invite`, `support`, `about`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`")
+     .addField("General Commands [8]", "`invite`, `support`, `stats`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`")
      .addField("Moderation Commands [11]", "`kick`, `ban`, `lock`, `unlock`, `lockall`, `unlockall`, `prefix`, `clear`, `unbanall`, `mute`, `lang`")
      .addField("Security Commmands [4]", "`anti`, `settings`, `punishment`, `whitelist`")
      .setFooter(`by : ${message.author.tag}`)
