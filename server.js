140f7d464b778ec5949ef34f6054b7d45bc19046
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-10 21:15:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -105,6 +105,14 @@ bot.on("message", async message => {
   }
 });
 ////////////
+bot.on("guildCreate" async message => {
+
+const thanksEmbed = new Discord.MessageEmbed()			
+                        .setAuthor("Thank you for adding me to your guild !")
+			.setDescription("To configure me,\n type **s!help** and look at the **Security** commands!.")
+			.setColor("#2c2f33")
+                        .setTimestamp();
+		guild.owner.send(thanksEmbed);
 
 //=============================== - [ antimention ] - ===================================//
 
