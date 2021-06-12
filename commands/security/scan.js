42a170eb9a3cb4046f692c3373da10ff74d967a4
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-12 23:58:00 +0300

    Create scan.js

---
+++ commands/security/scan.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+module.exports = {
+  name: "scan",
+  aliases: [],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  guilOwnerOnly: true,
+  cooldown: 6000,
+  run: async (bot, message, args, dev) => {
+    const { guild, channel } = message,
+    kickData = [],
+      banData = [],
+      pruneData = [],
+      safeData = [];
+    guild.roles.cache.forEach(role => {
+      if (role.permissions.has("KICK_MEMBERS") && role.permissions.has("BAN_MEMBERS")) {
+        pruneData.push(role.id);
+      } else if (role.permissions.has("KICK_MEMBERS")) {
+        kickData.push(role.id);
+      } else if (role.permissions.has()) {
+        pruneData.push(role.id);
+      } else {
+        safeData.push(role.id);
+      }
+      let x = "";
+      pruneData.forEach(pruneD => {
+        x += x + "<@&" + pruneD + ">\n";
+      })
+      channel.send(x)
+    })
+  }
+}
