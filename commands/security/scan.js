191a4ae34f9ac3bd7c5611a3feb4572da6463cc3
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-12 20:10:03 +0300

    Update scan.js

--- commands/security/scan.js
+++ commands/security/scan.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
   name: "scan",
   aliases: [],
-  enabled: false,
+  enabled: true,
   memberPermissions: ["SEND_MESSAGES"],
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   ownerOnly: false,
