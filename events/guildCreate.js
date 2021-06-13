e414c46aa76a24aea9b07ba3fb84a736e189f2fa
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-06-14 02:13:16 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,14 +4,14 @@ module.exports = class {
  		const thanksEmbed = new Discord.MessageEmbed()			
                         .setAuthor("Thank you for adding me to your guild !")
  			.setDescription("To configure me,\n type **s!help** and look at the administration commands!.")
- 			.setColor("BLACK")
+ 			.setColor("#2c2f33")
  			.setTimestamp();
  		guild.owner.send(thanksEmbed).catch(() => {});
 
 
  		const text = "✅ **__Joined New Guild__** \n **Guild Name** = "+guild.name+" \n **Guild Owner Name** = " + `${guild.owner.user.username}` + " \n **Guild Owner ID** = " + `${guild.owner.id}` + "\n **Guild Member Size** = "+guild.memberCount+" \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
  		const logsEmbed = new Discord.MessageEmbed()
- 			.setColor("BLACK")
+ 			.setColor("#2c2f33")
  			.setDescription(text);
  		bot.channels.cache.get("850465538306998272").send(logsEmbed);     
  }};
