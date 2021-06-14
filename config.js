227b28ec38abd203bc90f1569ff94d7280850403
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-14 23:10:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.Color = `#2c2f33`;
-exports.Image = `https://cdn.discordapp.com/attachments/819295562468687912/854088216840110080/image0.gif`;
-exports.Footer = `Code By sakran`;
-exports.Author = `code by sakran`, `https://cdn.discordapp.com/attachments/819295562468687912/854088216840110080/image0.gif`;
+exports.Image = ``;
+exports.Footer = ``;
+exports.Author = ``;
