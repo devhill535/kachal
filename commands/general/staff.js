17476cb936ce9050a76d2d79b3acaa7ec83a6914
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-14 22:11:50 +0300

    Update staff.js

--- commands/general/staff.js
+++ commands/general/staff.js
@@ -1,3 +1,19 @@
+const fs = require("fs");
+const Discord = require("discord.js");
+const { Color } = require("../../config.js");
+module.exports = {
+  name: "staff",
+  aliases: ["staffs"],
+  description: "To show you all command of the bot",
+  usage: ["s!help","s!help <command>"],
+  category: ["General"],
+  enabled: true,            
+  memberPermissions: [ "SEND_MESSAGES" ],            
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],        
+  ownerOnly: false,            
+  cooldown: 1000,
+  run: async (bot, message, args) => {
+   
 await message.guild.members.fetch();
 		const administrators = message.guild.members.cache.filter((m) => m.permissions.has("ADMINISTRATOR") && !m.user.bot);
 		const moderators = message.guild.members.cache.filter((m) => !administrators.has(m.id) && m.permissions.has("MANAGE_MESSAGES") && !m.user.bot);
