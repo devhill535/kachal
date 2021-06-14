38f4190aa77465ca9c6c96465cd4c8ee2793ac96
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-14 23:01:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.Color = `#2c2f33`;
-exports.Image = ``;
-exports.Footer = ``;
-exports.Author = ``;
-exports.Token = ``;
+exports.Image = `https://cdn.discordapp.com/attachments/819295562468687912/854088216840110080/image0.gif`;
+exports.Footer = `Code By sakran`;
+exports.Author = `code by sakran`, `https://cdn.discordapp.com/attachments/819295562468687912/854088216840110080/image0.gif`;
+exports.Token = `NzExMzI4NTcwMzc0NjE5MjA3.XsBaWw.xSGyRpsuS3lH1sd4PCobud6pYdI`;
