7347e0c638182ec982f92fb3f14261cf8a0fa3ef
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-17 01:56:52 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -24,7 +24,7 @@ module.exports = {
 [ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)
 `)
      .addField("General - (8)", "`invite`, `support`, `stats`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`")
-     .addField("Moderation - (11)", "`kick`, `ban`, `lock`, `unlock`, `lockall`, `unlockall`, `setprefix`, `clear`, `unbanall`, `mute`, `setlang`")
+     .addField("Moderation - (11)", "`kick`, `ban`, `clear`, `lock`, `unlock`, `lockall`, `unlockall`, `mute`, `unbanall`, `setprefix`, `setlang`")
      .addField("Security - (4)", "`anti`, `settings`, `punishment`, `whitelist`")
      .setFooter(Footer)
    message.channel.send(embed)
