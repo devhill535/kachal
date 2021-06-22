3ae96b081fac9869e00297d439b3191053c10906
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-23 00:43:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,10 +31,6 @@ bot.catagories = fs.readdirSync("./commands/");
   require(`./handler/${handler}`)(bot);
 });
 /**/
-
-let guild = await Guild.findOne({ guildID: message.guild.id });
-
-/**/
 let util = require("util"),
   readdir = util.promisify(fs.readdir);
 
@@ -100,7 +96,7 @@ bot.on("messageUpdate", (message, newMessage) => {
 bot.on("message", async message => {
   if (message.content.startsWith(`<@${bot.user.id}>`)) {
      
-    message.channel.send(`Hello: **${message.author.tag}**, my prefix on this server is \`${guild.prefix}\` Use \`${guild.prefix}help\` to get the list of the commands!`);
+    message.channel.send(`Hello: **${message.author.tag}**, my prefix on this server is \`${prefix}\` Use \`${prefix}help\` to get the list of the commands!`);
   }
 });
 
