9da4b8162f7a6c276ec85c0f12e473af8cbe6b08
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-26 01:23:28 +0300

    Update scan.js

--- commands/security/scan.js
+++ commands/security/scan.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports = {
   name: "scan",
   aliases: [],
-  enabled: true,
+  enabled: false,
   memberPermissions: ["SEND_MESSAGES"],
   botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
   ownerOnly: false,
