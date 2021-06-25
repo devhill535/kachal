6ff5244287c515b5847dfaad9d2b72fb8cde6e8b
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-25 23:39:51 +0300

    Update premium.js

--- commands/general/premium.js
+++ commands/general/premium.js
@@ -15,7 +15,7 @@ module.exports = {
   run: async (bot, message, args, dev) => {
  
 
-   let premium = new Discord.MessageEmbed()
+   let embed = new Discord.MessageEmbed()
    .serColor(Color)
    .setDescription(`For security, a premium version that gives you more powerful features, you can get a premium for only $2.00, click here:[**server**](https://discord.gg/UUbBQubh7V), to get the premium:
 **Anti-Spam**
@@ -24,6 +24,6 @@ A new and powerful spam detect system stop anyone from trying to raid your serve
 He can unban all people until they are banned form the server
 **Setlang**
 You can change the language of the bot to the languages currently available in the bot`)
-     message.channel.send(premium)
+     message.channel.send(embed);
     }
 }
