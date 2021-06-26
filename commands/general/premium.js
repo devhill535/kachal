ec4b23de5d677d1c97e610c45827ab2042ec480d
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-26 22:11:38 +0300

    Update premium.js

--- commands/general/premium.js
+++ commands/general/premium.js
@@ -15,7 +15,7 @@ module.exports = {
 const embed = new Discord.MessageEmbed()
  .setColor(Color)
  .setDescription(`
-Security has a premium version which gives you more powerful features, you can get premium for only **$2.00**, click here: [Server](https://discord.gg/UUbBQubh7V) , here is what you get from premium\n**AntiSpam**\nA new and powerful spam detect system stop anyone from trying to raid your server, the spam system will be able to kick or band a member and it will clear all the messages.\n**Unban all**\nHe can unban all people until they are banned form the server.\n**Setlang**\nYou can change the language of the bot to the languages ​​currently available in the bot.`)
+Security has a premium version which gives you more powerful features, you can get premium for only **$2.00**, click here: [Server](https://discord.gg/UUbBQubh7V) , here is what you get from premium\n**AntiSpam**\nA new and powerful spam detect system stop anyone from trying to raid your server, the spam system will be able to kick or band a member and it will clear all the messages.\n**Unban all**\nHe can unban all people until they are banned form the server.\n**Setlang**\nYou can change the language of the bot to the languages ​​currently available in the bot.\n**Show logs**\nWhen editing Anticommand in Log shows who changed it.`)
 
 message.channel.send(embed);
     }
