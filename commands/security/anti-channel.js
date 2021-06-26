df4381ad24a4efb004025753e4f9ef3cfd2a38ce
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-26 21:21:12 +0300

    Update anti-channel.js

--- commands/security/anti-channel.js
+++ commands/security/anti-channel.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 const { Color } = require("../../config.js");
 
 module.exports = {
-  name: "anti",
-  aliases: ["antichannel"],
+  name: "antichannel",
+  aliases: ["anti-channel"],
   description: "Prevent others from crating or deleting channels",
-  usage: ["s!anti channel [number/on/off]"],
+  usage: ["s!antichannel [number/on/off]"],
   category: ["Security"],
   enabled: true,
   memberPermissions: ["SEND_MESSAGES"],
@@ -15,10 +15,10 @@ module.exports = {
   guilOwnerOnly: true,
   cooldown: 3000,
   run: async (bot, message, args) => {
-  if (args[1] === "channel") {
+ 
    let guild = await Guild.findOne({ guildID: message.guild.id });
-     let num = args[2];
-    if (args[2] === "on") {
+     let num = args[1];
+    if (args[1] === "on") {
       guild.channel.onoff = "on";
       guild.channel.user = message.author.id
       guild.save();
@@ -26,7 +26,7 @@ module.exports = {
         .setColor(Color)
         .setDescription(`<a:true:854842599444709386> You have **Enable** antichannel`);
       return message.channel.send(embed);
-     } else if (args[2] === "off") {
+     } else if (args[1] === "off") {
          guild.channel.onoff = "off";
          guild.save();
       const embed1 = new Discord.MessageEmbed()
@@ -50,5 +50,4 @@ module.exports = {
 `);
     return message.channel.send(embed3);
   }
-}
-}
+};
