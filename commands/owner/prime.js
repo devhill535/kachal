62454457ffdcdc662452e8aa7875bc9a7c8683a5
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-06-27 22:28:35 +0300

    Create prime.js

---
+++ commands/owner/prime.js
@@ -0,0 +1,71 @@
+
+const moment = require("moment-timezone");
+const parseInt = require("ms")
+const day = require("dayjs")
+module.exports = {
+  name: "prime",
+  aliases: ["p","prime"],
+  enabled: true,			
+  memberPermissions: [ "SEND_MESSAGES" ],			
+  botPermissions: [ "SEND_MESSAGES" ],		
+  ownerOnly: true,			
+  cooldown: 1000,
+  prime: false,
+  run: async (bot, message, args, dev) => {
+
+    if (args[1]==="time") {
+      let data = await Prime.findOne({Guild: message.guild.id });
+      if (data) {
+        if (data.log==="enable") {
+        let time = day(data.time);
+       if(!data.time) return message.channel.send(`your srrver don't have prime bot `)   
+
+        
+      message.channel.send(`prime bot in this server end in ${time}`)
+    
+          } else {
+            message.channel.send("this server don't have a prime bot") 
+          }
+        }
+      if (!data) {
+          message.channel.send("this server don't have a prime bot")
+        }
+    } else
+      
+      if (args[1]=== "add") {
+     if (!args[2]) return message.reply(`please specify guild id`)
+       if(!bot.guilds.cache.has(args[2])) return message.reply(`your guild id is invalid`)
+    
+      
+    let time = day(args[3]).valueOf();;
+              let data = await Prime.findOne({ Guild: args[2]});
+      if (data) {
+        data.time = 0;
+        data.log = "enable";
+        data.save()
+      }
+      if(!data) { Prime.create({
+        Guild: args[2],
+        time: time,
+        log: "enable",
+        Permanent: false
+      }); } 
+        let time0 = day(time)
+
+      message.channel.send(`prime bot in this server ${time0}`)
+      
+      }else if (args[1]==="remove"){
+      if (!args[2]) return message.reply(`pleade give me a guild id`)
+        
+        let data = await Prime.findOne({ Guild: args[2]})
+      
+        if(data) {
+         
+          data.log = "enable";
+          data.delete()
+        }
+  message.channel.send(`prime bot on server removed`)
+      }
+  
+  }
+}
