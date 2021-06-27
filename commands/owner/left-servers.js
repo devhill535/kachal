853b9223b96c624c6ec597afaf6682106c25b32b
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-28 00:35:31 +0300

    Update left-servers.js

--- commands/owner/left-servers.js
+++ commands/owner/left-servers.js
@@ -15,7 +15,7 @@ module.exports = {
 let args = message.content.split(" ")[1];
 if (!args) message.channel.send(new Discord.MessageEmbed()
 .setColor(Color)
-.setDescrpition(`Please type server id`));
+.setDescrpition(`Please type server id`))
 let Guild = bot.guilds.cache.get(args);
 if (!Guild) return message.channel.send(new Discord.MessageEmbed()
 .setColor(Color)
@@ -26,4 +26,4 @@ message.channel.send(new Discord.MessageEmbed()
 .setDescription(`Done Leave **${Guild.name}**`))
 
   }
-};
+}
