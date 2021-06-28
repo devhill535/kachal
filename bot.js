96aae05f2707876bd6d64f1790d178802aacc89b
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-28 20:10:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ bot.on("message", async message => {
 })*/
 //=============================== - [ antispam ] - ===================================//
 
-const usersMap = new Map();
+/*const usersMap = new Map();
 const LIMIT = 5;
 const TIME = 6000;
 const DIFF = 7000;
@@ -210,6 +210,74 @@ bot.on("message", async message => {
       });
     }
   }
-});
+});*/
 
+const usersMap = new Map();
+const LIMIT = 5;
+const TIME = 6000;
+const DIFF = 7000;
+
+bot.on("message", async message => {
+  if (!message.channel.guild) return;
+  let guild = await Guild.findOne({ guildID: message.guild.id });
+  if (!guild) { Guild.create({ guildID: message.guild.id }); }
+  if (guild) {
+    if (guild.spam.onoff === "off") return;
+    let Ww = await Owner.findOne({ ownerCode: "738478465870987425" });
+    if (Ww.worldWhitelist.find((c) => c.type === message.author.id)) return;
+    if (message.author.id === message.guild.ownerID) return console.log("owner");
+    if (guild.whitelist.find((c) => c.type === message.author.id))
+      return console.log("whitelist");
+    let pun = guild.punishment;
+    if (message.author.bot) return;
+    if (usersMap.has(message.author.id)) {
+      const userData = usersMap.get(message.author.id);
+      const { lastMessage, timer } = userData;
+      const difference = message.createdTimestamp - lastMessage.createdTimestamp;
+      let msgCount = userData.msgCount;
+      if (difference > DIFF) {
+        clearTimeout(timer);
+        userData.msgCount = 1;
+        userData.lastMessage = message;
+        userData.timer = setTimeout(() => {
+          usersMap.delete(message.author.id);
+        }, TIME);
+        usersMap.set(message.author.id, userData);
+      } else {
+        ++msgCount;
+        if (parseInt(msgCount) >= LIMIT) {
+          if (pun === "") {
+            if (!message.member.bannable) return console.log(`${message.member.username} I can't ban this man`);
+            message.channel.guild.members.cache
+              .get(message.author.id)
+              .ban()
+            message.channel.bulkDelete(msgCount, true);
+          } else {
+            message.channel.guild.members.cache
+              .get(message.author.id)
+              .kick()
+              .then(k => {
+                guild.owner.send(
+                  `**⇏<@${message.author.id}> is kicked because spaming in channel**`
+                );
+              });
+            message.channel.bulkDelete(msgCount, true);
+          }
+        } else {
+          userData.msgCount = msgCount;
+          usersMap.set(message.author.id, userData);
+           } 
+        }
+       } else {
+      let fn = setTimeout(() => {
+        usersMap.delete(message.author.id);
+      }, TIME);
+      usersMap.set(message.author.id, {
+        msgCount: 1,
+        lastMessage: message,
+        timer: fn
+      });
+    }
+}
+});
 //==================================================================//
