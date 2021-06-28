eec85e1672e86f484d58887f37861910499d71e9
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-28 15:24:36 +0300

    Update roleDelete.js

--- events/roleDelete.js
+++ events/roleDelete.js
@@ -39,9 +39,9 @@ module.exports = class {
           }
 } else 
  if (guildData.punishment === "removerole") {
-        channel.guild.members.cache.get(user2.id).roles.cache.forEach(r => {
+        role.guild.members.cache.get(user2.id).roles.cache.forEach(r => {
           if (r.name !== "@everyone") {
-            channel.guild.members.cache.get(user2.id).roles.remove(r.id)
+            role.guild.members.cache.get(user2.id).roles.remove(r.id)
           }
         }).then(bruhlolxd => {
           let embed = new Discord.MessageEmbed()
