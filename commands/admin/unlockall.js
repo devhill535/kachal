5ea34020437973a87c4000f744017f010538bb78
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-29 16:50:12 +0300

    Update unlockall.js

--- commands/admin/unlockall.js
+++ commands/admin/unlockall.js
@@ -17,7 +17,8 @@ module.exports = {
    
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setDescription(bot.reva.get(data.lang, "admin","unlockall_message"));
+      .setDescription(bot.reva.get(data.lang, "admin","unlockall_message", { 
+          channels: `${message.guild.channels.cache.size}`}));
       message.channel.send(embed);
 
     message.guild.channels.cache.filter(c => c.name).forEach(async channel => {
