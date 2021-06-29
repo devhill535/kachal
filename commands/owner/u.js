1a3f255b8b0a57d56539e58d5dd00c8e8fc0b199
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-30 00:25:44 +0300

    Create u.js

---
+++ commands/owner/u.js
@@ -0,0 +1,22 @@
+
+const fs = require("fs");
+const { Color } = require("../../config.js");
+const Discord = require("discord.js");
+const ownerid = "738478465870987425";
+
+module.exports = {
+  name: "u",
+  enabled: true,
+  memberPermissions: [ "SEND_MESSAGES" ],
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+  ownerOnly: true,
+  cooldown: 6000,
+  run: async (bot, message, args) => {
+    if (message.author.id == ownerid) {
+
+ message.channel.send(new Discord.MessageEmbed()
+.setColor(Color)
+.setDescrpition(`${bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`))
+  }
+}
+}
