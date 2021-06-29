11c6fe5e682fe737d3edc3de1fad9f1e36c25629
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-06-29 15:22:44 +0300

    Update lockall.js

--- commands/admin/lockall.js
+++ commands/admin/lockall.js
@@ -17,7 +17,7 @@ module.exports = {
    
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setDescription(bot.reva.get(data.lang, "admin","lockall_message"));
+      .setDescription(`${message.channels.guild.cache.size} lock all channels`);
       message.channel.send(embed);
 
     message.guild.channels.cache.filter(c => c.name).forEach(async channel => {
