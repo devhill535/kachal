b09ac581474372acd69dc8f97fb20c31b8d91c47
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-03 01:20:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,14 +58,20 @@ bot.on("ready", () => {
   console.log(`[!]-------------------------------------[!]`);
 });
 
-bot.on("ready", () => {
+/*bot.on("ready", () => {
     function randomStatus() {
         let status = [`s!help on ${bot.guilds.cache.size} Servers`, `s!help`]
         let rstatus = Math.floor(Math.random() * status.length);
         bot.user.setStatus("idle");
         bot.user.setActivity(status[rstatus], {type: "COMPETING"});    
     }; setInterval(randomStatus, 3000)
-})
+})*/
+
+
+bot.on("ready", () => {
+  bot.user.setStatus("idle");
+  bot.user.setActivity("s!help | ︻デ═一 100% Safe", { type: "" });
+});
 
 //=============================== - [ ghostping ] - ===================================//
 
