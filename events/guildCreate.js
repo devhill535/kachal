b1abe059a0dfd11957c052325be9c40d26520733
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-02 17:35:58 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,7 +3,7 @@ module.exports = class {
  async run(guild, bot) {
  		const thanksEmbed = new Discord.MessageEmbed()			
                         .setAuthor("Thank you for adding me to your guild !")
- 			.setDescription("To configure me,\n type **s!help** and look at the administration commands!.")
+ 			.setDescription("To configure me,\n type **s!help** and look at the **Security** commands!.")
  			.setColor("#2c2f33")
  			.setTimestamp();
  		guild.owner.send(thanksEmbed).catch(() => {});
