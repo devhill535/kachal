a908f403b95dd78a9d73943534d8100c489ce403
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-03 00:12:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,6 +62,7 @@ bot.on("ready", () => {
     function randomStatus() {
         let status = [`s!help on ${bot.guilds.cache.size} Servers`, `s!help`]
         let rstatus = Math.floor(Math.random() * status.length);
+        bot.user.setStatus("idle");
         bot.user.setActivity(status[rstatus], {type: "COMPETING"});    
     }; setInterval(randomStatus, 3000)
 })
