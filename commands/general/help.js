aba4e808725486bdce01ba2217232a1ebb5a297b
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-03 22:56:55 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -21,7 +21,7 @@ module.exports = {
      .setImage(Image)
      .setTitle(bot.reva.get(data.lang, "general","help_embed"))
      .setDescription(`
-[ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)
+[ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)\n\nPing Bot ${bot.ws.ping}\nTotal Commands 25\n\n
 `)
      .addField("General - (9)", "`invite`, `support`, `stats`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`, `premium`")
      .addField("Moderation - (11)", "`kick`, `ban`, `clear`, `unbanall`, `mute`, `lock`, `unlock`, `lockall`, `unlockall`, `setlang`, `setprefix`")
