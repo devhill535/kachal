e7b4a8d3b71b3ce32f91d0b471ffabe5ed6b90eb
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-04 20:10:17 +0300

    Update userinfo.js

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -16,11 +16,13 @@ module.exports = {
   run: async (bot, message, args) => {
 
 let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+///
+let nickname = member.nickname !== undefined && member.nickname !== null ? member.nickname : "None";
+///
 
-  
-   
-    let nickname = member.nickname !== undefined && member.nickname !== null ? member.nickname : "None";
-
+let flags = member.flags.toArray();
+    if(member.flags.toArray() < 1) flags = "None";
+    
 
       const embed = new MessageEmbed()
       .setColor(Color)
@@ -28,6 +30,7 @@ let member = await message.mentions.members.first() || message.guild.members.cac
       .addField("Username", member.user.tag, true)
       .addField("Nickname", `${nickname}`, true)
       .addField("User Id", `${member.id}`, true)
+      .addField("Flags", `${flags}`, true)
       .addField("Join", member.joinedAt.toDateString())
       .addField("Creation", member.user.createdAt.toDateString())
       .addField("Roles", `Role ${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).length}: <@&${member._roles.join('> <@&')}>`)
