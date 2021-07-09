78749a2ae3143d1ddad53d7fb3a6ae4224530ce9
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-10 00:53:06 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,32 +1,40 @@
 const Discord = require("discord.js")
 module.exports = class {
-  async run(guild) {
+  async run(member) {
     try {
-      const entry1 = await guild.fetchAuditLogs({ type: "MEMBER_KICK" })
-        .then(audit => audit.entries.first());
-      const user2 = entry1.executor;
-      
-      const guildData = await Guild.findOne({ guildID: guild.id });
-      if (!guildData) { Guild.create({ guildID: guild.id }); }
-      const memberData = await User.findOne({ guildID: guild.id, userID: user2.id });
-      if (!memberData) { User.create({ guildID: guild.id, userID: user2.id }); }
-      if (guildData.kick.onoff === "off") return;
-      if (user2.id === guild.ownerID) return;
-      if (guildData.whitelist.find((c) => c.type === user2.id)) return;
-      let Ww = await Owner.findOne({ ownerCode: "738478465870987425" });
-      if (Ww.worldWhitelist.find((c) => c.type === user2.id)) return;
-      if (guildData.kick.lmite === 1) {
-        let member = await guild.members.fetch(user2.id)
-        const embed = new Discord.MessageEmbed()
-          .setColor("#fc0303")
-          .setThumbnail(guild.iconURL())
-          .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
-          .setDescription(`${user2.username} kick 1 member don’t worry i taked the action!`);
-        const embed2 = new Discord.MessageEmbed()
-          .setColor("#fc0303")
-          .setThumbnail(guild.iconURL())
-          .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
-          .setDescription(`${user2.username} kick 1 member i can't take the action!`);
+      if (member.guild) {
+          const { guild } = member,
+          user = member;
+          const entry1 = await member.guild
+            .fetchAuditLogs()
+            .then(audit => audit.entries.first());
+          if (entry1.action === "MEMBER_KICK") {
+            const entry2 = await member.guild
+              .fetchAuditLogs({ type: "MEMBER_KICK" })
+              .then(audit => audit.entries.first());
+            const user2 = entry1.executor;
+           //Fix some err 
+           const guildData = await Guild.findOne({ guildID: guild.id });
+           if (!guildData) { Guild.create({ guildID: guild.id }); }
+           const memberData = await User.findOne({ guildID: guild.id, userID: user2.id });
+           if (!memberData) { User.create({ guildID: guild.id, userID: user2.id }); }
+           if (guildData.kick.onoff === "off") return;
+           if (user2.id === guild.ownerID) return;
+           if (guildData.whitelist.find((c) => c.type === user2.id)) return;
+           let Ww = await Owner.findOne({ ownerCode: "738478465870987425" });
+           if (Ww.worldWhitelist.find((c) => c.type === user2.id)) return;
+           if (guildData.kick.lmite === 1) {
+             let member = await guild.members.fetch(user2.id)
+             const embed = new Discord.MessageEmbed()
+               .setColor("#fc0303")
+               .setThumbnail(guild.iconURL())
+               .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
+               .setDescription(`${user2.username} kick 1 member don’t worry i taked the action!`);
+             const embed2 = new Discord.MessageEmbed()
+               .setColor("#fc0303")
+               .setThumbnail(guild.iconURL())
+               .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
+               .setDescription(`${user2.username} kick 1 member i can't take the action!`);
 
         if (guildData.punishment === "ban") {
           if (member.bannable) {
