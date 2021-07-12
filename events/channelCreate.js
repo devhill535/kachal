2071868e1dd40a4f12ead99398d4e16e161c54d7
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-12 16:20:35 +0300

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -19,13 +19,16 @@ module.exports = class {
         let member = await guild.members.fetch(user2.id)
         const embed = new Discord.MessageEmbed()
           .setColor("#fc0303")
-          .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
           .setThumbnail(guild.iconURL())
           .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
           .setDescription(`${user2.username} created or deleted 1 channels don’t worry i taked the action!`);
         const embed2 = new Discord.MessageEmbed()
           .setColor("#fc0303")
-          .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
+          .setThumbnail(guild.iconURL())
+          .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
+          .setDescription(`${user2.username} created or deleted 1 channels i can't take the action!`);
+         const embed3 = new Discord.MessageEmbed()
+          .setColor("#fc0303")
           .setThumbnail(guild.iconURL())
           .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
           .setDescription(`${user2.username} created or deleted 1 channels i can't take the action!`);
@@ -40,37 +43,16 @@ module.exports = class {
             await guild.owner.send(embed2).catch(err => {})
           }
         } else 
- if (guildData.punishment === "removerole") {
+      if (guildData.punishment === "removerole") {
         channel.guild.members.cache.get(user2.id).roles.cache.forEach(r => {
           if (r.name !== "@everyone") {
             channel.guild.members.cache.get(user2.id).roles.remove(r.id)
           }
         }).then(bruhlolxd => {
-          let embed = new Discord.MessageEmbed()
-          .setColor("#fc0303")
-          .setAuthor("")
-          .setThumbnail(guild.iconURL())
-          .setTitle(`Actions in the server **${guild.name}**`)
-          .addField("User", user2.tag)
-        guild.owner.send(embed)
+       embed.addField ("RemoveRole", `Name: ${user2.username}\nTag : ${user2.tag}\nID: ${user2.id}`)
+        await guild.owner.send(embed3)
         }).catch(err => {
-          let embed2 = new Discord.MessageEmbed()
-          .setColor("#fc0303")
-          .setAuthor("")
-          .setThumbnail(guild.iconURL())
-          .setTitle(`Actions in the server **${guild.name}**`)
-          .addField("User", user2.tag)
-          guild.owner.send(embed2)
-        })
-      //db.add(`${role.guild.id}_${user.id}_rolecreate`, 1)
-       ///let pog = db.get(`${role.guild.id}_${user.id}_rolecreate`)
-       let embed3 = new Discord.MessageEmbed()
-          .setColor("#fc0303")
-          .setAuthor("")
-          .setThumbnail(guild.iconURL())
-          .setTitle(`Actions in the server **${guild.name}**`) 
-          .addField("User", user2.tag)
-        guild.owner.send(embed3)
+         
 } else
 
 if (guildData.punishment === "kick") {
@@ -95,16 +77,19 @@ if (guildData.punishment === "kick") {
           let member = await guild.members.fetch(user2.id)
           const embed = new Discord.MessageEmbed()
             .setColor("#fc0303")
-            .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
             .setThumbnail(guild.iconURL())
             .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
             .setDescription(`${user2.username} created or deleted ${guildData.channel.lmite} channels don’t worry i taked the action!`);
           const embed2 = new Discord.MessageEmbed()
             .setColor("#fc0303")
-            .setAuthor("The Av Development", "https://media.discordapp.net/attachments/829446297115033610/831559964165406771/image0.gif")
             .setThumbnail(guild.iconURL())
             .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
             .setDescription(`${user2.username} created or deleted ${guildData.channel.lmite} channels i can't take the action!`);
+          const embed3 = new Discord.MessageEmbed()
+            .setColor("#fc0303")
+            .setThumbnail(guild.iconURL())
+            .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
+             .setDescription(`${user2.username} created or deleted 1 channels i can't take the action!`);
 
 
           if (guildData.punishment === "ban") {
@@ -116,7 +101,19 @@ if (guildData.punishment === "kick") {
               embed2.addField("Can't ban", `Name: ${user2.username}\nTag : ${user2.tag}\nID: ${user2.id}`)
               await guild.owner.send(embed2).catch(err => {})
             }
-          } else if (guildData.punishment === "kick") {
+          } else
+         if (guildData.punishment === "removerole") {
+        channel.guild.members.cache.get(user2.id).roles.cache.forEach(r => {
+          if (r.name !== "@everyone") {
+            channel.guild.members.cache.get(user2.id).roles.remove(r.id)
+          }
+        }).then(bruhlolxd => {
+       embed.addField ("RemoveRole", `Name: ${user2.username}\nTag : ${user2.tag}\nID: ${user2.id}`)
+        await guild.owner.send(embed3)
+        }).catch(err => {
+         
+} else
+   if (guildData.punishment === "kick") {
             if (member.kickable) {
               await member.kick({ reason: `Create or Delete ${guildData.channel.lmite} channels` })
               embed.addField("Kick", `Name: ${user2.username}\nTag : ${user2.tag}\nID: ${user2.id}`)
