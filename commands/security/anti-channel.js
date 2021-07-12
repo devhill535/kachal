d93dbf660337ae82d197e9f094965116769ae12a
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-13 00:25:17 +0300

    Update anti-channel.js

--- commands/security/anti-channel.js
+++ commands/security/anti-channel.js
@@ -24,14 +24,14 @@ module.exports = {
       guild.save();
       const embed = new Discord.MessageEmbed()
         .setColor(Color)
-        .setDescription(`<a:true:854842599444709386> You have **Enable** antichannel`);
+        .setDescription(`<a:true:854842599444709386> The **AntiChannel** system is enabled correctly!`);
       return message.channel.send(embed);
      } else if (args[1] === "off") {
          guild.channel.onoff = "off";
          guild.save();
       const embed1 = new Discord.MessageEmbed()
         .setColor(Color)
-        .setDescription(`<a:false:854842600351334440> You have **Disable** antichannel`);
+        .setDescription(`<a:false:854842600351334440> The **AntiChannel** system is disabled correctly!`);
       return message.channel.send(embed1);
     }
     if (isNaN(num) || parseInt(num) < 1) {
