1dcb75120f517519d823eb852d378c8901a07787
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-13 16:19:32 +0300

    Update unban.js

--- commands/admin/unban.js
+++ commands/admin/unban.js
@@ -1,3 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports = {
+  name: "unban",
+  aliases: [""],
+  description: "You can kick a member, or multiple members using this command",
+  usage: ["s!kick [@User]"],
+  category: ["Moderation"],
+  enabled: true,			  
+  memberPermissions: [ "BAN_MEMBERS" ],			
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS","BAN_MEMBERS" ],		
+  ownerOnly: false,			
+  cooldown: 6000,
+  run: async (client, message, args, dev) => {
+
 const userId = args[0];
 
     if (!userId) {
