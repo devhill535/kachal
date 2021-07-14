b6ef4fc630fdff8b5b4329ef50675369b3fd27f8
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-14 04:55:42 +0300

    Update disable-ghostping.js

--- commands/security/disable-ghostping.js
+++ commands/security/disable-ghostping.js
@@ -5,11 +5,8 @@ module.exports = {
   description: "Disable Anti Ghost Ping Module",
   enabled: true,
   run: async(client, message, args) => {
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`You don't have permission to use this command`)
-    if(!message.channel.permissionsFor(message.guild.me).has(["SEND_MESSAGES"])) return message.member.send(`:x: Cancelled the command. Reason: Require \`Send Messages\` permission !`)
-    Schema.findOne({ Guild: message.guild.id }, async(err, data) => {
-      if(!data) return message.channel.send(`:x: Anti Ghost Ping Module is disabled already`)
-      data.delete()
+    Schema.findOne({ Guild: message.guild.id }, async(err) => {
+   
       message.channel.send(`Anti Ghost Ping Module has been disabled. To enable Anti Ghost Ping Module, run \`enable-ghotstping\` command (includes server prefix)`)
     })
   }
