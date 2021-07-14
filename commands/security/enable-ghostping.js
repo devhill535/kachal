1cafd4123d3ae7325a305314edffe73bb4f24f4f
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-14 04:52:07 +0300

    Update enable-ghostping.js

--- commands/security/enable-ghostping.js
+++ commands/security/enable-ghostping.js
@@ -1,11 +1,9 @@
 const Schema = require('../../models/ghostping')
 
 module.exports = {
-  name: 'enable-ghostping',
+  name: "ghostping-on",
   description: "Enable Anti Ghost Ping Module",
-  aliases: ['egp'],
-  timeout: '10000',
-  requirePermission: "Manage Server",
+  enabled: true,
   run: async(client, message, args) => {
     if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`You don't have permission to use this command`)
     if(!message.channel.permissionsFor(message.guild.me).has(["SEND_MESSAGES"])) return message.member.send(`:x: Cancelled the command. Reason: Require \`Send Messages\` permission !`)
