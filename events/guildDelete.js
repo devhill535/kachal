c0f3c6101180312d03cbde7ebdfb7a4e41b34f4f
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-16 14:49:27 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -9,8 +9,8 @@ const Discord = require("discord.js")
  		guild.owner.send(thanksEmbed).catch(() => {});
 
 
- 		const text = "❎ **__Leaving Old Guild__** \n **Guild Name** = "+guild.name+" \n **Guild Owner Name** = " + `${guild.owner.user.username}` + " \n **Guild Owner ID** = " + `${guild.owner.id}` + " \n **Guild Bots Size** ("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
- 		const logsEmbed = new Discord.MessageEmbed()
+ 		const text = "Leaving guild **"+guild.name+"** guild member size **"+guild.members.cache.filter((m) => !m.user.bot).size+"** membres, guild bot size("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
+                const logsEmbed = new Discord.MessageEmbed()
  			.setColor("#2c2f33")
  			.setDescription(text);
  		bot.channels.cache.get("853773470102061087").send(logsEmbed);     
