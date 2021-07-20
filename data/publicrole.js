7a3a49da6150758800fb0837795973e6d8045524
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-20 05:23:21 +0300

    Delete publicrole.js

--- data/publicrole.js
+++
@@ -1,9 +0,0 @@
-schema = mongoose.Schema({
-  guildID: String,
-  aRoles: { type: Array, default: [] },
-  
-  pStatus: { onoff : { type: String, default:  "off" }
-  },
-  
-});
-module.exports = mongoose.model("PublicRole", schema)
