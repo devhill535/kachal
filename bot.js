2537779a16bb0118abdd1dbc9e5d71800a39864d
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-20 05:02:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,8 @@ mongoose.connect("mongodb+srv://antivandalism:reman1234@cluster0.prbzz.mongodb.n
 global.Guild = require("./data/guild.js");
 global.User = require("./data/user.js");
 global.Lang = require("./data/lang.js");
-global.Prime = require("./data/prime.js")
+global.Prime = require("./data/prime.js");
+global.PublicRole = require("./data/publicrole.js");
 global.Owner = require("./data/owner.js");
 bot.commands = new Collection();
 bot.aliases = new Collection();
