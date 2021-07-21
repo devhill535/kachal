18749fdd9cff7ab30a6a47e6b5d0e40cba2e3a29
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-21 13:29:59 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -15,12 +15,24 @@ module.exports = {
   cooldown: 2000,
   run: async (bot, message, args, dev, data) => {
 
+   let kod1 = bot.makeid(6);
 
-   const embed = new Discord.MessageEmbed()
+   let sorgu = new MessageButton()
+    .setLabel(kod1)
+    .setStyle("blurple")
+    .setID(kod1)
+
+const correctButton = new Discord.MessageEmbed()
+	.setTitle("The correct button has been selected.").setColor("GREEN")
+	.setAuthor(message.author.username, message.author.avatarURL({ dynamic: true }))
+	.setDescription(`You have successfully bumped for server **${message.guild.name}**.`)
+mesaage.channel.send(correctButton);
+
+  /* const embed = new Discord.MessageEmbed()
   .setColor(Color)
   .setTitle(bot.reva.get(data.lang, "general","invite"))
   .setDescription(`[Anti Vandalism Premium](https://discord.com/api/oauth2/authorize?client_id=813131436265046068&permissions=8&scope=bot)\n[Anti Vandalism](https://discord.com/api/oauth2/authorize?client_id=711328570374619207&permissions=8&scope=bot)`)
 
- message.channel.send(embed);
+ message.channel.send(embed);*/
      }
  }
