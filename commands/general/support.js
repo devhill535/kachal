ecbd6b3bb7a784131097227c983b7c06084d4378
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-21 21:12:13 +0300

    Update support.js

--- commands/general/support.js
+++ commands/general/support.js
@@ -12,6 +12,11 @@ module.exports = {
   cooldown: 2000,
   run: async (bot, message, args, dev) => {
 
-message.channel.send(`This is a server support **Anti Vandalism** if you need help, enter the server - \n https://discord.gg/QaqmPG2WZX`);
+let butn = new MessageButton()
+  .setStyle('url')
+  .setURL('https://discord.gg/QaqmPG2WZX') 
+  .setLabel('Support Server!')
+
+message.channel.send('This is a server support **Anti Vandalism** if you need help, enter the server', butn);
     } 
 }
