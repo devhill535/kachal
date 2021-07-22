b9238f5fa1819e5dd6bc7672635fad6d366b99cf
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-22 13:10:23 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const Discord = require("discord.js");
+const { MessageMenuOption, MessageMenu } = require("discord-buttons")
 const { Color, Image, Footer, Author } = require("../../config.js");
 module.exports = {
   name: "help",
@@ -16,17 +17,65 @@ module.exports = {
    
     if (!args[1]) {
 
-let button1 = new MessageButton()
-  .setStyle('url')
-  .setURL('https://i8.ae/53lTv') 
-  .setLabel('Invite!')
+  let cmduser = message.author;
+  let menuoptions = [ 
+    {
+      value: "role1", description: "Get Role 1",
+      replymsg: "This is the message for Role 1", emoji: "❌" //optional
+    }, {
+      value: "role2",  description: "Get Role 2",
+      replymsg: "This is the message for Role 2", emoji: "✅" //optional
+    } 
+  ]
+  //define the selection
+  let Selection = new MessageMenu()
+    .setID('MenuSelection') 
+    .setMaxValues(1) //OPTIONAL, this is how many values you can have at each selection
+    .setMinValues(1) //OPTIONAL , this is how many values you need to have at each selection
+    .setPlaceholder('Click me to make a Selection!');  //message in the content placeholder
+  menuoptions.forEach(option => {
+    let row = new MessageMenuOption()
+      .setLabel(option.label ? option.label : option.value)
+      .setValue(option.value) 
+      .setDescription(option.description) 
+      .setDefault() 
+    if(option.emoji) row.setEmoji(option.emoji) 
+    Selection.addOption(row)
+  })
+  //define the embed
+  let MenuEmbed = new Discord.MessageEmbed()
+    .setColor("BLUE")
+    .setAuthor("Bot Help", client.user.displayAvatarURL())
+    .setDescription("***Select what you need in the `Selection` down Below!***")
+  //send the menu msg
+  let menumsg = await message.channel.send(MenuEmbed, Selection)
+  //function to handle the menuselection
+  function menuselection(menu) {
+    let menuoptiondata = menuoptions.find(v=>v.value == menu.values[0])
+    menu.reply.send(menuoptiondata.replymsg, true);
+  }
+  //Event
+  bot.on('clickMenu', (menu) => {
+    if (menu.message.id === menumsg.id) {
+      if (menu.clicker.user.id === cmduser.id) menuselection(menu);
+      else menu.reply.send(`:x: You are not allowed to do that! Only: <@${cmduser.id}>`, true);
+    }
+  });
+}}
 
-let button2 = new MessageButton()
-  .setStyle('url')
-  .setURL('https://i8.ae/53lTv') 
-  .setLabel('Support!')
 
-  let embed = new Discord.MessageEmbed()
+
+
+
+
+
+
+
+
+
+
+
+ /* let embed = new Discord.MessageEmbed()
      .setColor(Color)
      .setAuthor(Author)
      .setImage(Image)
@@ -39,9 +88,8 @@ let button2 = new MessageButton()
      .addField("Config", "`setprefix`, `setlang`")
      .addField("Security", "`settings`, `punishment`, `whitelist`, `anti`, `logs`")
      .setFooter(Footer)
- message.lineReplyNoMention({buttons: [button1, button2], embed: embed})
- /// return message.lineReplyNoMention(embed);
-       } else {
+ return message.channel.send(embed);*/
+ } else {
       let  command = args[1]
       if (bot.commands.has(command) || 
       bot.aliases.has(command)) {  
