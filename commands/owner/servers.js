7b5e357aa0d24c9d3b4308336f822647e23548d1
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-07-30 14:44:14 +0300

    Update servers.js

--- commands/owner/servers.js
+++ commands/owner/servers.js
@@ -1,11 +1,24 @@
+const fs = require("fs");
+const { Color } = require("../../config.js");
+const Discord = require("discord.js");
+const ownerid = "738478465870987425";
 
-
+module.exports = {
+  name: "servers",
+  enabled: true,
+  memberPermissions: [ "SEND_MESSAGES" ],
+  botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+  ownerOnly: true,
+  cooldown: 6000,
+  run: async (bot, message, args) => {
+    if (message.author.id == ownerid) {
 
 message.channel.send(new Discord.MessageEmbed()
-.setColor("GREEN")
+.setColor(Color)
 .setTitle(`**Servers List**`)
 .setDescription(client.guilds.cache.map(c => `**- ${c.name} | ${c.memberCount} Members
   ID - ${c.id}
   **`))
     }
   }
+}
