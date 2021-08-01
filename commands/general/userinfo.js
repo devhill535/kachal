fd30b807430e134ff7171ca46f04c46c4ac8c88d
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-01 20:52:56 +0300

    Update userinfo.js

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -30,9 +30,7 @@ module.exports = {
   cooldown: 2000,
   run: async (bot, message, args) => {
 
-const member = bot.members.cache.find(e => e.id == args[0]) || message.mentions.members.first();
-
-///let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.guild.members.cache.find(r => r.id.toLowerCase() === args.join(' ').toLocaleLowerCase()) || message.member;
+let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username == args[0]) || message.guild.members.cache.find(r => r.displayName == args[0]) || message.guild.members.cache.find(r => r.id == args[0]) || message.member;
 ///
 let nickname = member.nickname !== undefined && member.nickname !== null ? member.nickname : "None";
 ///
