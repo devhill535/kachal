382b121ff30ee38bfdca31d940bc09ba7ddebb0e
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-01 20:23:59 +0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,14 +1,6 @@
 const Discord = require("discord.js")
  module.exports = class {
  async run(guild, bot) {
- 		const thanksEmbed = new Discord.MessageEmbed()			
-                        .setAuthor("I'm leaving in your server !!")
- 			.setDescription("If you have a report join this server https://discord.gg/DnEQJdTUsS")
- 			.setColor("#2c2f33")
- 			.setTimestamp();
- 		guild.owner.send(thanksEmbed).catch(() => {});
-
-
  		const text = "Leaving guild **"+guild.name+"** guild member size **"+guild.members.cache.filter((m) => !m.user.bot).size+"** membres, guild bot size("+guild.members.cache.filter((m) => m.user.bot).size+" bots)";
                 const logsEmbed = new Discord.MessageEmbed()
  			.setColor("#2c2f33")
