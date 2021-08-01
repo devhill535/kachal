917079747db07cb896550f0d198de64f2e5c28e1
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-01 20:28:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ const cmd = require("node-cmd");
 const { I18n } = require("locale-parser");
 bot.reva = new I18n({ defaultLocale: "en" });
 
-bot.login("ODEzMTMxNDM2MjY1MDQ2MDY4.YDK1qQ.3B2NJzGQ3vIVQ7ivtmvHK3xbWSk")
+bot.login("ODEzMTMxNDM2MjY1MDQ2MDY4.YDK1qQ.Yj-mAudgLO0c04ita59t1LeHMxY")
 global.mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://antivandalism:reman1234@cluster0.prbzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log("Connected to the Mongodb database.");
