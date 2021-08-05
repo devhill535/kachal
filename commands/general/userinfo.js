763549c23d8c388ecafc7606f55fa5a4390bc6f1
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-05 12:52:13 +0300

    Update userinfo.js

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -30,7 +30,7 @@ module.exports = {
   cooldown: 2000,
   run: async (bot, message, args) => {
 
-///let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username == args[0]) || message.guild.members.cache.find(r => r.displayName == args[0]) || message.guild.members.cache.find(r => r.id == args[0]) || message.member;
+let member = await message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username == args[0]) || message.guild.members.cache.find(r => r.displayName == args[0]) || message.guild.members.cache.find(r => r.id == args[0]) || message.member;
 ////
 let nickname = member.nickname !== undefined && member.nickname !== null ? member.nickname : "None";
 ///
