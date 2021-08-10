695de788d9c20c765e869c45a68938bcbbe3a154
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-10 05:56:48 +0300

    Update premium.js

--- commands/general/premium.js
+++ commands/general/premium.js
@@ -17,7 +17,7 @@ const embed = new Discord.MessageEmbed()
  .setDescription(`
 Security has a premium version which gives you more powerful features, you can get premium for only **$2.00**, click here: [Server](https://discord.gg/KBbsApGpVZ) , here is what you get from premium\n**AntiSpam**\nA new and powerful spam detect system stop anyone from trying to raid your server, the spam system will be able to kick or band a member and it will clear all the messages.\n**Unban all**\nHe can unban all people until they are banned form the server.\n**Setlang**\nYou can change the language of the bot to the languages ​​currently available in the bot.\n**Logs**\nWhen editing Anticommand in Log shows who changed it.`)
 
-return message.lineReplyNoMention(embed);
+return message.channel.send(embed);
 
     }
 }
