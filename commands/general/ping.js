7ad03651f9d123d19c79d61a31e7e1f12c7782e5
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-14 01:49:47 +0300

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -22,25 +22,20 @@ module.exports = {
         });
 
         date = Date.now();
-        let embed = new Discord.MessageEmbed()
-        .setDescription(``)
-        message.channel.send(embed).then(msg =>{
-setTimeout(function(){
-msg.edit("Loading.")
-},700)
-setTimeout(function(){
-msg.edit("Loading..")
-},1000)
-setTimeout(function(){
-msg.edit("Loading..")
-},1300)
-setTimeout(function(){
-msg.edit(new Discord.MessageEmbed()
-                   .setDescription(`<:ping:828370866537758790> Bot: ${bot.ws.ping}ms \n<:api:836017379330228234> Discord API: ${Date.now() - date}ms \n<:file:836016653908705312> DB: ${ping_db}ms`)
-                   .setColor(Color))
-},6000)
 
-               
+        let pong = new Discord.MessageEmbed()
+            .setColor(Color)
+            .setDescription('Pong?')
+
+        return message.channel.send({ embed: pong })
+            .then(msg => {
+
+                let embed = new Discord.MessageEmbed()
+                   .setDescription(`<:ping:828370866537758790> Bot: ${bot.ws.ping}ms \n<:api:836017379330228234> Discord API: ${Date.now() - date}ms \n<:file:836016653908705312> DB: ${ping_db}ms`)
+                   .setColor(Color)
+
+                return msg.edit({ embed })
+
             })
     }
 }
