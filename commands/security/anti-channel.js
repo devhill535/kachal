b9d52a17e0886b5f5e5f0750c7abe066608044f9
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-08-16 15:17:03 +0300

    Update anti-channel.js

--- commands/security/anti-channel.js
+++ commands/security/anti-channel.js
@@ -20,7 +20,7 @@ module.exports = {
      let num = args[1];
     if (args[1] === "on") {
       guild.channel.onoff = "on";
-      guild.channel.user = message.author.id
+      guild.channel.user = message.author.tag
       guild.save();
       const embed = new Discord.MessageEmbed()
         .setColor(Color)
@@ -41,7 +41,7 @@ module.exports = {
         );
       return message.channel.send(embed2);
     }
-    guild.channel.user = message.author.id
+    guild.channel.user = message.author.tag
     guild.channel.lmite = num;
     guild.save();
     const embed3 = new Discord.MessageEmbed()
