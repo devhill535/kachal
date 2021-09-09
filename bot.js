c25a88134c39502927d5cc5420362fdad8aca439
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-09 23:24:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,8 @@ const Discord = require("discord.js");
 const { MessageButton } = require("discord-buttons");
 const bot = new Discord.Client(); 
 require('discord-buttons')(bot);
-const { Token, Color, Image, Footer, Author } = require("./config.js");
-const fs = require("fs");
+const { Color, Image, Footer, Author } = require("./config.js");
+const fs = require("fs"); 
 const request = require("request");
 const prefix = "s!";
 const { Collection, MessageEmbed } = require("discord.js");
@@ -15,7 +15,7 @@ const cmd = require("node-cmd");
 const { I18n } = require("locale-parser");
 bot.reva = new I18n({ defaultLocale: "en" });
 
-bot.login("ODEzMTMxNDM2MjY1MDQ2MDY4.YDK1qQ.c552N8qNK1VvhrWmdSWeu5poQ0w")
+bot.login("NzExMzI4NTcwMzc0NjE5MjA3.XsBaWw.xSGyRpsuS3lH1sd4PCobud6pYdI")
 global.mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://antivandalism:reman1234@cluster0.prbzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log("Connected to the Mongodb database.");
@@ -30,7 +30,7 @@ global.Owner = require("./data/owner.js");
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.cooldowns = new Collection();
-bot.catagories = fs.readdirSync("./commands/"); 
+bot.catagories = fs.readdirSync("./commands/");
 ["command"].forEach(handler => {
   require(`./handler/${handler}`)(bot);
 });
@@ -61,28 +61,91 @@ bot.on("ready", () => {
 });
 
 bot.on("ready", async () => {
-  console.log(`bot now is ready!`);
+  //let channel = bot.channels.cache.get("880441113574539365");
+  //channel.send(new Discord.MessageEmbed().setColor(Color).setTimestamp().setThumbnail(bot.user.displayAvatarURL()).setTitle("Anti Vandalism Status").addField("Prefix", "`s!`").addField("Status", "<:enable:840230134899671060> Online").addField("Servers", `${bot.guilds.cache.size}`));
   await bot.user.setStatus("online");
-  await bot.user.setActivity(`${prefix}help`, { type: "COMPETING" });
+  await bot.user.setActivity(`${prefix}help - www.antivandalism.ga`, { type: "PLAYING" });
  
  });
+/////
+
+/*bot.on("message", async message => {
+if (message.content.startsWith(prefix + "menu")) {
+   
+        let option1 = new MessageMenuOption()
+            .setLabel("Option 1")        
+            .setValue("Option 1")
+            .setDescription("This will give u the option 1")
+            .setDefault()
+        let option2 = new MessageMenuOption()
+            .setLabel("Option 2")
+            .setValue("Option 2")
+            .setDescription("This will give u the option 2")
+            .setDefault()
+        let option3 = new MessageMenuOption()
+            .setLabel("Option 3")
+            .setValue("Option 3")
+            .setDescription("This will give u the option 3")
+            .setDefault()
+        let option4 = new MessageMenuOption()
+            .setLabel("Option 4")
+            .setValue("Option 4")
+            .setDescription("This will give u the option 4")
+            .setDefault()
+        let selection = new MessageMenu()
+            .setID("Selection")
+            .setMaxValues(1)
+            .setMinValues(1)
+            .setPlaceholder("Click me to make a Selection!")
+            .addOption(option1)
+            .addOption(option2)
+            .addOption(option3)
+            .addOption(option4)
+        let embed = new Discord.MessageEmbed()
+        .setColor(Color).setTitle("Help Menu")
+        let menumsg = await message.channel.send(embed, selection)
+        function menuselection(menu) {
+            switch(menu.values[0]) {
+                case "Option 1": 
+                    menu.reply.send(new Discord.MessageEmbed().addField("teztysh", `test`))
+                break;
+                case "Option 2": 
+                    menu.reply.send("the reply for option 2", true)
+                break;
+                case "Option 3": 
+                    menu.reply.send("the reply for option 3", true)
+                break;
+                case "Option 4": 
+                    menu.reply.send("the reply for option 4", true)
+                break;
+            }
+        }
+        bot.on("clickMenu", (menu) => {
+            if(menu.message.id == menumsg.id) {
+                if(menu.clicker.user.id == message.author.id) menuselection(menu)
+                else menu.reply.send("You are not allowed to pick something", true)
+            }
+        })
+   }
+})*/
+////
+
+/*bot.on('ready', () => {
+let channel = bot.channels.cache.get("880491394068803604");
+    if (channel) channel.join();
+});*/
 
 ////
-bot.on("clickButton", async (button) => {
+
+/*bot.on("clickButton", async (button) => {
  console.log(button.id);
-});
+});*/
+
 //=============================== - [ ghostping ] - ===================================//
 
-/*bot.on("messageDelete", (message) => {
-
-if (!message.channel.guild) return;
-  let guild = await Guild.findOne({ guildID: message.guild.id });
-  if (!guild) { Guild.create({ guildID: message.guild.id }); }
-  if (guild) {
-    if (guild.ghostping.onoff === "off") return;
-    let Ww = await Owner.findOne({ ownerCode: "738478465870987425" });
-    if (Ww.worldWhitelist.find((c) => c.type === message.author.id)) return;
-    if (message.author.bot) return;
+/*
+bot.on("message", (message) => {});
+bot.on("messageDelete", (message) => {
 if (message.mentions.users.first()) {
     message.channel.send(new Discord.MessageEmbed().setColor(Color) .setTitle("Ghost Ping Detected!")
             .setDescription(`**${message.author}** just pinged **${
@@ -92,10 +155,22 @@ if (message.mentions.users.first()) {
          message.mentions.users.first().username
        }**||`));
    }
-  }
+});
+bot.on("messageUpdate", (message, newMessage) => {
+ if (message.mentions.users.first()) {
+    if (newMessage.mentions.users.first()) return;
+    message.channel.send(new Discord.MessageEmbed().setColor(Color).setTitle("Ghost Ping Detected!")
+            .setDescription(`Ghost Ping Found!\n${message.author} just pinged ${
+         message.mentions.users.first().username
+       } and then someone deleted the message!`)
+            .addField("Deleted message content", `||${
+         message.mentions.users.first().username
+       }||`));
+    }
 });*/
 
 //=============================== - [ antimention ] - ===================================//
+
 
 /*bot.on('message', message => {
   let non = ['@here','@everyone']
@@ -106,93 +181,12 @@ if (message.mentions.users.first()) {
       message.member.roles.add(roled)
     }
     if(message.deletable){
-      message.channel.send('').then(m => m.delete({timeout:5000}))
+      message.channel.send(new Discord.MessageEmbed().setColor(Color).setTitle("Anti Mention").setDescription(`<a:false:836711508246659109> Please Don‘t send \`@here\` & \`@everyone\``)).then(m => m.delete({timeout:5000}))
       message.delete()
     }
   }
 })*/
-
 //=============================== - [ antispam ] - ===================================//
-
-/*const usersMap = new Map();
-const LIMIT = 5;
-const TIME = 6000;
-const DIFF = 7000;
-
-bot.on("message", async message => {
-  if (!message.channel.guild) return;
-  let guild = await Guild.findOne({ guildID: message.guild.id });
-  if (!guild) { Guild.create({ guildID: message.guild.id }); }
-  if (guild) {
-    if (guild.spam.onoff === "off") return;
-   let Ww = await Owner.findOne({ ownerCode: "738478465870987425" });
-    if (Ww.worldWhitelist.find((c) => c.type === message.author.id)) return;
-  if (message.author.id === message.guild.ownerID) return console.log("owner");
-    if (guild.whitelist.find((c) => c.type === message.author.id))
-      return console.log("whitelist");
-    let pun = guild.punishment;
-    if (message.author.bot) return;
-    if (usersMap.has(message.author.id)) {
-      const userData = usersMap.get(message.author.id);
-      const { lastMessage, timer } = userData;
-      const difference = message.createdTimestamp - lastMessage.createdTimestamp;
-      let msgCount = userData.msgCount;
-      if (difference > DIFF) {
-        clearTimeout(timer);
-        userData.msgCount = 1;
-        userData.lastMessage = message;
-        userData.timer = setTimeout(() => {
-          usersMap.delete(message.author.id);
-        }, TIME);
-        usersMap.set(message.author.id, userData);
-      } else {
-        ++msgCount;
-        if (parseInt(msgCount) >= LIMIT) {
-          if (pun === "ban") {
-            if (!message.member.bannable) return console.log(`${message.member.username} I can't ban this man`);
-            message.channel.guild.members.cache
-              .get(message.author.id)
-              .ban()
-            message.channel.bulkDelete(msgCount, true);
-          } else if (pun === "kick") {
-            if (!message.member.kickable) return console.log(`${message.member.username} I can't kick this man`);
-            message.channel.guild.members.cache
-              .get(message.author.id)
-              .kick()
-              .then(k => {
-               guild.owner.send(
-                  `**⇏<@${message.author.id}> is kicked because spaming in channel**`
-                );
-              });
-            message.channel.bulkDelete(msgCount, true);
-          } else {
-            message.channel.guild.members.cache
-              .get(message.author.id)
-              .kick()
-              .then(k => {
-               guild.owner.send(
-                  `**⇏<@${message.author.id}> is kicked because spaming in channel**`
-                );
-              });
-            message.channel.bulkDelete(msgCount, true);
-          }
-        } else {
-          userData.msgCount = msgCount;
-          usersMap.set(message.author.id, userData);
-        }
-      }
-    } else {
-      let fn = setTimeout(() => {
-        usersMap.delete(message.author.id);
-      }, TIME);
-      usersMap.set(message.author.id, {
-        msgCount: 1,
-        lastMessage: message,
-        timer: fn
-      });
-    }
-  }
-});*/
 
 const usersMap = new Map();
 const LIMIT = 5;
@@ -229,7 +223,7 @@ bot.on("message", async message => {
         ++msgCount;
         if (parseInt(msgCount) >= LIMIT) {
           if (pun === "") {
-            if (!message.member.bannable) return console.log(`${message.member.username} I can't ban this man`);
+            if (!message.member.bannable) return console.log(``);
             message.channel.guild.members.cache
               .get(message.author.id)
               .ban()
@@ -239,9 +233,7 @@ bot.on("message", async message => {
               .get(message.author.id)
               .kick()
               .then(k => {
-                guild.owner.send(new Discord.MessageEmbed().setColor(Color).setDescription(
-                  `This user is kicked because spaming in channel\nTag: ${message.author.tag}\nID: ${message.author.id}`
-                ));
+                guild.owner.send({ content: `This user is kicked because spaming in channel` });
               });
             message.channel.bulkDelete(msgCount, true);
           }
@@ -262,4 +254,3 @@ bot.on("message", async message => {
     }
 }
 });
-//==================================================================//
