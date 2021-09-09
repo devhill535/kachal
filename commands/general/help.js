ad0fd875e2a5939fd82edd0be728951d2470e47e
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-09 23:31:07 +0300

    Update help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const Discord = require("discord.js");
+const { MessageButton, MessageActionRow } = require("discord-buttons");
 const { Color, Image, Footer, Author } = require("../../config.js");
 module.exports = {
   name: "help",
@@ -15,20 +16,41 @@ module.exports = {
   run: async (bot, message, args, dev, data) => {
    
     if (!args[1]) {
-  let embed = new Discord.MessageEmbed()
-     .setColor(Color)
-     .setAuthor(Author)
-     .setImage(Image)
-     .setTitle(bot.reva.get(data.lang, "general","help_embed"))
-     .setDescription(`
-[ Top.gg ](https://top.gg/bot/711328570374619207) - [ Invite ](https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&permissions=8&scope=bot) - [ Support ](https://discord.gg/QaqmPG2WZX) - [ Website ](https://zalmanti25.wixsite.com/my-site)
-`)
-     .addField("General", "`invite`, `support`, `stats`, `userinfo`, `ping`, `serverinfo`, `bots`, `vote`, `premium`")
-     .addField("Moderation", "`kick`, `ban`, `purge`, `unbanall`, `mute`, `lock`, `unlock`, `lockall`, `unlockall`")
-     .addField("Config", "`setprefix`, `setlang`")
-     .addField("Security", "`settings`, `punishment`, `whitelist`, `anti`, `logs`")
-     .setFooter(Footer)
-   return message.lineReplyNoMention(embed);
+  let helpEmbed = new Discord.MessageEmbed()
+     helpEmbed.setColor(Color)
+     helpEmbed.setAuthor(Author)
+     helpEmbed.setImage(Image)
+     helpEmbed.setTitle(bot.reva.get(data.lang, "general","help_embed"))
+     helpEmbed.addField("General Section", "`invite`, `support`, `stats`, `serverinfo`, `ping`, `userinfo`, `bots`, `vote`, `premium`")
+     helpEmbed.addField("Moderation Section", "`kick`, `ban`, `purge`, `unbanall`, `mute`, `lock`, `unlock`, `lockall`, `unlockall`")
+     helpEmbed.addField("Config Section", "`setprefix`, `setlang`")
+     helpEmbed.addField("Security Section", "`settings`, `punishment`, `whitelist`, `anti`, `logs`")
+     helpEmbed.setFooter(Footer)
+
+      let button1 = new MessageButton()
+       .setStyle('url')
+       .setURL('https://discord.com/api/oauth2/authorize?client_id=711328570374619207&permissions=8&scope=bot') 
+       .setLabel('Invite Link')
+
+      let button2 = new MessageButton()
+       .setStyle('url')
+       .setURL('https://discord.gg/xZ3Sssr8qN') 
+       .setLabel('Server Support')
+
+      let button3 = new MessageButton()
+       .setStyle('url')
+       .setURL('https://top.gg/bot/711328570374619207') 
+       .setLabel('Top.gg')
+
+      let button4 = new MessageButton()
+       .setStyle('url')
+       .setURL('https://www.antivandalism.ga/') 
+       .setLabel('Website')
+
+      let row1 = new MessageActionRow()
+      .addComponents(button1, button2, button3, button4)
+
+   return message.channel.send({ embeds: [helpEmbed] ,row: [row1] });
        } else {
       let  command = args[1]
       if (bot.commands.has(command) || 
@@ -39,15 +61,15 @@ module.exports = {
         if ( command.enabled ) {
         ccmd = "<:enable:840230134899671060> Enabled"
         }
-      let embed = new Discord.MessageEmbed()
-      .setColor(Color) 
-      .setThumbnail(message.author.avatarURL())
-      .setTitle("**Help**")
-      .setDescription(command.description || command.name + " this command don't have a description")
-      .addField("**Usage**", "" + command.usage.join(", ") + "" )
-      .addField("**Category**", "" + command.category.join(", ") + "" )
-      .addField("**Command is**", ccmd);
-      message.channel.send(embed)
+      let help1 = new Discord.MessageEmbed()
+      help1.setColor(Color) 
+      help1.setThumbnail(message.author.avatarURL())
+      help1.setTitle("**Help**")
+      help1.setDescription(command.description || command.name + " this command don't have a description")
+      help1.addField("**Usage**", "" + command.usage.join(", ") + "" )
+      help1.addField("**Category**", "" + command.category.join(", ") + "" )
+      help1.addField("**Command is**", ccmd);
+      message.channel.send({ embeds: [help1] })
         }
     }
   }};
