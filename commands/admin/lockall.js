b919f77aa1d9e829318e522f59d846ba363697a2
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-13 05:18:32 +0300

    Update lockall.js

--- commands/admin/lockall.js
+++ commands/admin/lockall.js
@@ -17,7 +17,7 @@ module.exports = {
    
       const embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setDescription(`${message.guild.channels.cache.size} Has been locked`));
+      .setDescription(`${message.guild.channels.cache.size} Channels has been locked`));
       message.lineReplyNoMention(embed);
 
     message.guild.channels.cache.filter(c => c.name).forEach(async channel => {
