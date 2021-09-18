dcf2f984eeb0b6180b5a15fc1a561a150e5f4f05
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-19 01:05:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -225,9 +225,9 @@ const usersMap = new Map();
                .then(k => {
                  k.guild.owner.send(new Discord.MessageEmbed()
           .setColor(Color)
-          .setThumbnail(guild.iconURL())
-          .setTitle(`<:punishment:837867514947174431> Actions in the server **${guild.name}**`)
-          .setDescription(`${message.author.username} kicked because spaming in channel!`));
+          .setThumbnail(k.guild.iconURL())
+          .setTitle(`<:punishment:837867514947174431> Actions in the server **${k.guild.name}**`)
+          .setDescription(`${k.author.username} kicked because spaming in channel!`));
                });
              message.channel.bulkDelete(msgCount, true);
            }
