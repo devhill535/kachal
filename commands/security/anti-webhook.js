5df1d96b9f4a7186a585f8f87a5088f7b60feb8e
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-21 01:56:54 +0300

    Create anti-webhook.js

---
+++ commands/security/anti-webhook.js
@@ -0,0 +1,52 @@
+
+ const fs = require("fs");
+const Discord = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "antiwebhook",
+  aliases: ["anti-webhook"],
+  description: "Prevent others from crating or deleting webhooks",
+  usage: ["s!antiwebhook [number/on/off]"],
+  category: ["Security"],
+  enabled: true,
+  memberPermissions: ["SEND_MESSAGES"],
+  botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+  ownerOnly: false,
+  guilOwnerOnly: true,
+  cooldown: 3000,
+  run: async (bot, message, args) => {
+ 
+   let guild = await Guild.findOne({ guildID: message.guild.id });
+     let num = args[1];
+    if (args[1] === "on") {
+      guild.webhook.onoff = "on";
+      guild.save();
+      const embed = new Discord.MessageEmbed()
+        .setColor(Color)
+        .setDescription(`<a:true:854842599444709386> The **AntiWebhook** system is enabled correctly!`);
+      return message.channel.send(embed);
+     } else if (args[1] === "off") {
+         guild.webhook.onoff = "off";
+         guild.save();
+      const embed1 = new Discord.MessageEmbed()
+        .setColor(Color)
+        .setDescription(`<a:false:854842600351334440> The **AntiWebhook** system is disabled correctly!`);
+      return message.channel.send(embed1);
+    }
+    if (isNaN(num) || parseInt(num) < 1) {
+      const embed2 = new Discord.MessageEmbed()
+        .setColor(Color)
+        .setDescription(`error syntax <a:false:854842600351334440>\n ${guild.prefix}antiwebhook [on,off,<number>]`
+        );
+      return message.channel.send(embed2);
+    }
+    guild.webhook.lmite = num;
+    guild.save();
+    const embed3 = new Discord.MessageEmbed()
+      .setColor(Color)
+      .setDescription(`Successfully antiwebhook changed to **${guild.webhook.lmite}** <:punish:836022893691011092>
+`);
+    return message.channel.send(embed3);
+  }
+};
