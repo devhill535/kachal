ac3226ab8f2b7adf5ec3d55b2972bdf968b86852
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-21 00:25:20 +0300

    Update stats.js

--- commands/general/stats.js
+++ commands/general/stats.js
@@ -2,19 +2,10 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const { Color } = require("../../config.js");
 const ms = require('ms');
-///const { MessageButton, MessageActionRow } = require("discord-buttons");
-const { stripIndents } = require("common-tags");
-const moment = require("moment-timezone");
- function duration(ms) {
-    const sec = Math.floor((ms / 1000) % 60).toString();
-    const min = Math.floor((ms / (1000 * 60)) % 60).toString();
-    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString();
-    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString();
-    return `${days.padStart(1, "0")}-${hrs.padStart(2, "0")}-${min.padStart(
-      2,
-      "0"
-    )}-${sec.padStart(2, "0")}`;
-  }
+const moment = require("moment"); 
+require("moment-duration-format"); 
+const os = require('os') 
+const si = require('systeminformation'); 
 
 module.exports = {
   name: "stats",
@@ -29,29 +20,48 @@ module.exports = {
   cooldown: 2000,
   run: async (bot, message, args, dev) => {
  
-    const created = moment(bot.user.createdAt).format("YYYY-MM-DD");
-    
-     
-       let stats = new Discord.MessageEmbed()
-        .setTitle("Security information")
-        .setColor(Color)
-        .setThumbnail(bot.user.displayAvatarURL())
-        .addField("Bot Id",  `${bot.user.id}`)
-        .addField("Commands Count" `19`)
-        .addField("Bot Created At", `${created}`)
-        .addField("Ping", `${Math.round(bot.ws.ping)}ms`)
-        .addField("Uptime", `${ms(bot.uptime, { long: true })}`)
-        .addField("Servers Count", `${bot.guilds.cache.size}`)
 
-      
-  
-  /*let button1 = new MessageButton()
-       .setStyle('url')
-       .setURL('https://discord.com/api/oauth2/authorize?client_id=828270556758540348&permissions=8&scope=bot') 
-       .setLabel('Click here to invite Whoami bot')
-      
-      let row1 = new MessageActionRow()
-      .addComponents(button1)*/
+
+ const created = moment(bot.user.createdAt).format("YYYY-MM-DD");
+
+const duration1 = moment.duration(message.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]"); 
+
+const cpu = await si.cpu(); 
+ 
+
+
+const stats = new Discord.MessageEmbed() 
+
+.setColor(Color) 
+.setThumbnail(message.client.user.displayAvatarURL()) 
+.setTitle("Whoami Stats")
+.setDescription(`**= Statistics =**\n\n**• Servers** : ${bot.guilds.cache.size}\n**Bot** :${bot.user.id}\n**Commands Count** : 19\n**Bot Created At** : ${created}\n**Ping** : ${Math.round(bot.ws.ping)}ms
+        
+**= SYSTEM =** 
+
+**• Platfrom** : ${os.type} 
+
+**• Uptime** : ${duration1} 
+
+**• CPU** : 
+
+> **• Cores** : ${cpu.cores} 
+
+> **• Model** : ${os.cpus()[0].model} 
+
+> **• Speed** : ${os.cpus()[0].speed} MHz 
+
+**• MEMORY** : 
+
+> **• Total Memory** : ${(os.totalmem() / 1024 / 1024).toFixed(2)} Mbps 
+
+> **• Free Memory** : ${(os.freemem() / 1024 / 1024).toFixed(2)} Mbps 
+
+> **• Heap Total** : ${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} Mbps 
+
+> **• Heap Usage** : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} Mbps 
+
+`)
 
    message.channel.send(stats);
     }
