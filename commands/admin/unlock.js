32e10ac66825f1ae69d81f95abad39530d12b686
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-09-22 20:08:52 +0300

    Update unlock.js

--- commands/admin/unlock.js
+++ commands/admin/unlock.js
@@ -18,7 +18,7 @@ module.exports = {
         SEND_MESSAGES: true
       })
       .then(() => {
-        message.lineReplyNoMention(new Discord.MessageEmbed().setColor(Color).setDescription(`<#${message.channel.id}> Has been unlocked`
+        message.channel.send(new Discord.MessageEmbed().setColor(Color).setDescription(`<#${message.channel.id}> Has been unlocked`
      ));
      });
    }
