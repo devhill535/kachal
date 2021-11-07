c309d5c1808bea59f93bff2ae6741a9599548c9c
Sakran0 <85891532+Sakran0@users.noreply.github.com>
2021-11-07 04:58:49 +0300

    Update userinfo.js

--- commands/general/userinfo.js
+++ commands/general/userinfo.js
@@ -2,21 +2,6 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 const { Color } = require("../../config.js");
 
-const flags = {
-	DISCORD_EMPLOYEE: 'Discord Employee',
-	DISCORD_PARTNER: 'Discord Partner',
-	BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
-	BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
-	HYPESQUAD_EVENTS: 'HypeSquad Events',
-	HOUSE_BRAVERY: 'House of Bravery',
-	HOUSE_BRILLIANCE: 'House of Brilliance',
-	HOUSE_BALANCE: 'House of Balance',
-	EARLY_SUPPORTER: 'Early Supporter',
-	TEAM_USER: 'Team User',
-	SYSTEM: 'System',
-	VERIFIED_BOT: 'Verified Bot',
-	VERIFIED_DEVELOPER: 'Verified Bot Developer'
-};
 module.exports = {
   name: "userinfo",
   aliases: ["user"],
@@ -36,24 +21,21 @@ let nickname = member.nickname !== undefined && member.nickname !== null ? membe
 ///
 const bots = member.user.bot ? "True" : "False";
 ///
-if (member.premiumSince) {
+/*if (member.premiumSince) {
     boost = "Yes"
   } else {
     boost = "No"
-  }
+  }*/
 ///
 
-      const userFlags = member.user.flags.toArray();
+     
       const embed = new MessageEmbed()
       .setColor(Color)
       .setThumbnail(member.user.displayAvatarURL())
-      .addField("Username", `${member.user.username}`, true)
-      .addField("Discriminator", `${member.user.discriminator}`, true)
+      .addField("Username", `${member.user.tag}`, true)
       .addField("Nickname", `${nickname}`, true)
       .addField("User Id", `${member.id}`, true)
       .addField("Is Bot", `${bots}`, true)
-      .addField("Booster", `${boost}`, true)
-      .addField("Flags", `${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`, true)
       .addField("Join", member.joinedAt.toDateString())
       .addField("Creation", member.user.createdAt.toDateString())
       .addField("Roles", `${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).length} Roles: <@&${member._roles.join('> <@&')}>`)
